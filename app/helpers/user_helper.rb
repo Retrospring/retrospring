07656775553958a85d11e72cb0f75872b38ef219
nilsding <nilsding@nilsding.org>
2014-11-11 07:09:36 +0100

    added a helper!

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,9 @@
 module UserHelper
+  # Decides what user name to show.
+  # @return [String] The user name
+  def user_screen_name(user)
+    return APP_CONFIG['anonymous_name'] if user.nil?
+    return user.display_name unless user.display_name.blank?
+    user.screen_name
+  end
 end
