e39f82882bef48142e507624907d8d68ee6ac247
nilsding <nilsding@nilsding.org>
2014-11-13 13:02:47 +0100

    link

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,9 +1,10 @@
 module UserHelper
   # Decides what user name to show.
   # @return [String] The user name
-  def user_screen_name(user, anonymous=false)
+  def user_screen_name(user, anonymous=false, url=true)
     return APP_CONFIG['anonymous_name'] if user.nil? || anonymous
-    return user.display_name unless user.display_name.blank?
-    user.screen_name
+    name = user.display_name.blank? ? user.screen_name : user.display_name
+    return link_to(name, show_user_profile_path(current_user.screen_name)) if url
+    name
   end
 end
