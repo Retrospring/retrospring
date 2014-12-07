2d44eeef60597cacc1cca60f0caefc902f6299cc
nilsding <nilsding@nilsding.org>
2014-12-07 14:29:47 +0100

    added validation to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,6 @@ class Comment < ActiveRecord::Base
   belongs_to :answer
   validates :user_id, presence: true
   validates :answer_id, presence: true
+
+  validates :content, length: { maximum: 160 }
 end
