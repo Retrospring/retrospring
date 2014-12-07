d4115538b1437b88f458e9eebb26bdcef0e8fa2f
nilsding <nilsding@nilsding.org>
2014-12-07 11:33:14 +0100

    changed user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do |u|
     u.screen_name { Faker::Internet.user_name 0..16, %w(_) }
     u.email { Faker::Internet.email }
-    u.password { Faker::Internet.password }
+    u.password { "P4s5w0rD" }
     u.display_name { Faker::Name.name }
   end
 end
