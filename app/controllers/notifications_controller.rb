b79d4f30aca5b051233fc0f230e8a2a7781309e3
nilsding <nilsding@nilsding.org>
2014-12-12 17:54:13 +0100

    oy vey

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,6 @@
 class NotificationsController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
   end
 end
--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,4 +1,6 @@
 class PublicController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
     @timeline = Answer.all.reverse_order.paginate(page: params[:page])
     respond_to do |format|
