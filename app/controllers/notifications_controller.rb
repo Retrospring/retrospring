b0a2072f961c6380e06ae14b8bbd5346cf6ed0e6
nilsding <nilsding@nilsding.org>
2014-12-14 15:24:59 +0100

    made notification tabs work

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,5 +3,10 @@ class NotificationsController < ApplicationController
 
   def index
     @type = params[:type]
+    @notifications = if @type == 'all'
+                       Notification.for(current_user)
+                     else
+                       Notification.for(current_user).where('LOWER(target_type) = ?', @type)
+                     end
   end
 end
