6471e3baa5383343e0f3496d448dad9985aedeae
nilsding <nilsding@nilsding.org>
2014-12-15 19:19:26 +0100

    fixed answers not being deleted

--- app/controllers/ajax/answer_controller.rb
+++ app/controllers/ajax/answer_controller.rb
@@ -16,7 +16,7 @@ class Ajax::AnswerController < ApplicationController
     if answer.user == current_user
       Inbox.create!(user: answer.user, question: answer.question, new: true)
     end # TODO: decide what happens with the question
-    Notification.denotify self.question.user, answer
+    Notification.denotify answer.question.user, answer
     answer.destroy
 
     @status = :okay
