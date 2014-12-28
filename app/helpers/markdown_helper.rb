0e4d7444787fef742899b4dc312bcbe1d911c6f3
nilsding <nilsding@nilsding.org>
2014-12-28 19:06:49 +0100

    added Markdown to answers

---
+++ app/helpers/markdown_helper.rb
@@ -0,0 +1,21 @@
+module MarkdownHelper
+
+  def markdown(content)
+    md = Redcarpet::Markdown.new(FlavoredMarkdown,
+                                 filter_html: true,
+                                 escape_html: true,
+                                 no_images: true,
+                                 no_styles: true,
+                                 safe_links_only: true,
+                                 xhtml: false,
+                                 hard_wrap: true,
+                                 no_intra_emphasis: true,
+                                 tables: true,
+                                 fenced_code_blocks: true,
+                                 autolink: true,
+                                 disable_indented_code_blocks: true,
+                                 strikethrough: true,
+                                 superscript: true)
+    Sanitize.fragment(md.render(content), EVIL_TAGS).html_safe
+  end
+end
---
+++ app/services/flavored_markdown.rb
@@ -0,0 +1,26 @@
+class FlavoredMarkdown < Redcarpet::Render::HTML
+  include Rails.application.routes.url_helpers
+
+  def preprocess(text)
+    wrap_mentions(text)
+  end
+
+  def wrap_mentions(text)
+    text.gsub! /(^|\s)(@\w+)/ do
+      "#{$1}[#{$2}](#{show_user_profile_path $2.tr('@', '')})"
+    end
+    text
+  end
+
+  def header(text, _header_level)
+    paragraph text
+  end
+
+  def paragraph(text)
+    "<p>#{text}</p>"
+  end
+
+  def raw_html(raw_html)
+    Rack::Utils.escape_html raw_html
+  end
+end
