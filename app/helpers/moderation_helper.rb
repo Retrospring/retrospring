7bfa5132dd88d90336c2a6e0a22181861f6d3f22
nilsding <nilsding@nilsding.org>
2014-12-28 20:05:30 +0100

    added link to the reported object

--- app/helpers/moderation_helper.rb
+++ app/helpers/moderation_helper.rb
@@ -1,2 +1,18 @@
 module ModerationHelper
+  # @param report [Report]
+  def content_url(report)
+    target = report.target
+    case report.type
+      when 'Reports::Answer'
+        show_user_answer_path target.user.screen_name, target.id
+      when 'Reports::Comment'
+        show_user_answer_path target.answer.user.screen_name, target.answer.id
+      when 'Reports::Question'
+        show_user_question_path 'user', target.id
+      when 'Reports::User'
+        show_user_profile_path target.screen_name
+      else
+        '#'
+    end
+  end
 end
