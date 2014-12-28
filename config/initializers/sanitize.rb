7537331651fdc8763cae3e72abb60eed907f4d24
nilsding <nilsding@nilsding.org>
2014-12-28 19:12:28 +0100

    changed this

--- config/initializers/sanitize.rb
+++ config/initializers/sanitize.rb
@@ -1,5 +1,5 @@
 EVIL_TAGS = {
-  elements: %w(quote a p i strong em del pre code table tr td th br ul ol li hr),
+  elements: %w(blockquote a p i strong em del pre code table tr td th br ul ol li hr),
   attributes: {
     'a' => %w(href)
   },
