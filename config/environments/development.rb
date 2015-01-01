57c1650e045050f551b34b2271d46dfcde857b4b
nilsding <nilsding@nilsding.org>
2015-01-01 18:08:09 +0100

    added mailers to development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { host: 'justask.local' }
+  config.action_mailer.perform_deliveries = true
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
