88616f0280f7f81d6617c83f73676c910080833f
nilsding <nilsding@nilsding.org>
2015-01-01 17:56:52 +0100

    added another mailer thing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { host: APP_CONFIG['hostname'] }
   config.action_mailer.perform_deliveries = true
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
