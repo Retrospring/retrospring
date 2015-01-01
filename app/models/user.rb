e9ab8deb85544ff856839975b77e8623b6b65fea
nilsding <nilsding@nilsding.org>
2015-01-01 18:17:34 +0100

    :async

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :async, :registerable,
          :recoverable, :rememberable, :trackable,
          :validatable, :authentication_keys => [:login]
 
