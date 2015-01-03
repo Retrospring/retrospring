09b6a6bc102e6e3a5546de0f7bc301d543e3cc1d
nilsding <nilsding@nilsding.org>
2015-01-03 22:27:14 +0100

    made that stuff work

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,10 +5,6 @@ class Question < ActiveRecord::Base
 
   validates :content, length: { maximum: 255 }
 
-  before_create do
-    raise "User does not want to receive anonymous questions" if self.author_is_anonymous and !self.user.privacy_allow_anonymous_questions?
-  end
-
   before_destroy do
     user.decrement! :asked_count unless self.author_is_anonymous
   end
