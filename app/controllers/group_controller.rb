1c26af4c8caf98871728925f43227609a4bd9439
nilsding <nilsding@nilsding.org>
2015-01-17 19:07:31 +0100

    @group now holds the current group

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -2,6 +2,7 @@ class GroupController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @timeline = current_user.groups.find_by_name!(params[:group_name]).timeline.paginate(page: params[:page])
+    @group = current_user.groups.find_by_name!(params[:group_name])
+    @timeline = group.timeline.paginate(page: params[:page])
   end
 end
