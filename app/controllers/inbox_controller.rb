cb606f942a2868ca628b4b446b82fc6c00bf3384
Georg G <nilsding@nilsding.org>
2015-02-12 22:09:11 +0100

    fixed pagination for real

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -2,6 +2,10 @@ class InboxController < ApplicationController
   before_filter :authenticate_user!
 
   def show
-    @inbox = Inbox.where(user: current_user).order(:created_at).reverse_order
+    @inbox = Inbox.where(user: current_user).order(:created_at).reverse_order.paginate(page: params[:page])
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
