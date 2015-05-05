42b41a268b622cd58206920afd9176f5c263d2d0
pixeldesu <git@pixelde.su>
2015-05-05 18:09:26 +0200

    fix not shown ban reason

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_filter :banned?
 
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
       name = current_user.screen_name
       # obligatory '2001: A Space Odyssey' reference
       flash[:notice] = "I'm sorry, #{name}, I'm afraid I can't do that."
-      if current_user.ban_reason.nil?
+      unless current_user.ban_reason.nil?
         flash[:notice] += "\nBan reason: #{current_user.ban_reason}"
       end
       if not current_user.permanently_banned?
