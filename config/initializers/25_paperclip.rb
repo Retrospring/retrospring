0a34cc07b3495111d152b14eeab28838f0849193
Yuki <aaron.ahmed.eve@gmail.com>
2015-05-08 23:48:18 +0530

    Paperclip fog support, needs AWS/Rack testing before deployment! (Retrospring/bugs#48)

---
+++ config/initializers/25_paperclip.rb
@@ -0,0 +1,9 @@
+Paperclip::Attachment.default_options[:storage] = :fog
+Paperclip::Attachment.default_options[:fog_credentials] = {:provider => "Local", :local_root => "#{Rails.root}/public"}
+Paperclip::Attachment.default_options[:fog_directory] = "/system"
+Paperclip::Attachment.default_options[:fog_host] = "/system"
+unless APP_CONFIG["fog"].nil?
+  Paperclip::Attachment.default_options[:fog_credentials] = APP_CONFIG["fog"]["credentials"] unless APP_CONFIG["fog"]["credentials"].nil?
+  Paperclip::Attachment.default_options[:fog_directory] = APP_CONFIG["fog"]["directory"] unless APP_CONFIG["fog"]["directory"].nil?
+  Paperclip::Attachment.default_options[:fog_host] = APP_CONFIG["fog"]["host"] unless APP_CONFIG["fog"]["host"].nil?
+end
