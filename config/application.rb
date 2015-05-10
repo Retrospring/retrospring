4c6b009d23993adb6c9b558881e71204bd74163a
Georg G <nilsding@nilsding.org>
2015-05-10 02:57:27 +0200

    use Sidekiq for processing

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module Justask
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.autoload_paths += %W["#{config.root}/app/validators"]
+
+    # Use Sidekiq for background jobs
+    config.active_job.queue_adapter = :sidekiq
   end
 end
