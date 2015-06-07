b39f5177f40e266ab4d6997087e7e7fc7e1f63f5
Yuki <aaron.ahmed.eve@gmail.com>
2015-06-07 17:21:09 +0530

    9th time's the charm

--- app/api/api.rb
+++ app/api/api.rb
@@ -93,8 +93,4 @@ class API < Grape::API
     status 404
     {message: "Not found", status: 404}
   end
-
-
-  ::APP_FAKE_OAUTH = Doorkeeper::Application.new(name: "Web", description: "#{APP_CONFIG["site_name"]} on the internets", homepage: "http#{APP_CONFIG["https"] && "s" || ""}://#{APP_CONFIG["hostname"]}#{APP_CONFIG["port"] && APP_CONFIG["port"] != 80 && ":#{APP_CONFIG["port"]}" || ""}", deleted: false, scopes: "-1")
-  ::APP_FAKE_OAUTH.readonly!
 end
--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,13 @@ module Justask
 
     config.after_initialize do
       Doorkeeper::Application.send :include, Concerns::ApplicationExtension
+
+      begin
+        ::APP_FAKE_OAUTH = Doorkeeper::Application.new(name: "Web", description: "#{APP_CONFIG["site_name"]} on the internets", homepage: "http#{APP_CONFIG["https"] && "s" || ""}://#{APP_CONFIG["hostname"]}#{APP_CONFIG["port"] && APP_CONFIG["port"] != 80 && ":#{APP_CONFIG["port"]}" || ""}", deleted: false, scopes: "-1")
+        ::APP_FAKE_OAUTH.readonly!
+      rescue
+        # wahahahahahaluigi
+      end
     end
   end
 end
