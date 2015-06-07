569c2af45f90e549274e7029fe983d1496386674
Yuki <aaron.ahmed.eve@gmail.com>
2015-06-07 17:18:34 +0530

    WALUIGI STRIKES AGAIN

--- config/application.rb
+++ config/application.rb
@@ -35,9 +35,6 @@ module Justask
 
     config.after_initialize do
       Doorkeeper::Application.send :include, Concerns::ApplicationExtension
-
-      ::APP_FAKE_OAUTH = Doorkeeper::Application.new(name: "Web", description: "#{APP_CONFIG["site_name"]} on the internets", homepage: "http#{APP_CONFIG["https"] && "s" || ""}://#{APP_CONFIG["hostname"]}#{APP_CONFIG["port"] && APP_CONFIG["port"] != 80 && ":#{APP_CONFIG["port"]}" || ""}", deleted: false, scopes: "-1")
-      ::APP_FAKE_OAUTH.readonly!
     end
   end
 end
