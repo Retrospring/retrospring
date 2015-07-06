2c1e4cbe25b86473c42e7f4381d9200c0d3ee067
Yuki <aaron.ahmed.eve@gmail.com>
2015-07-07 03:49:31 +0530

    Remove test test

--- spec/api/sleipnir/user_api_spec.rb
+++ spec/api/sleipnir/user_api_spec.rb
@@ -1,9 +1,5 @@
 require 'spec_helper'
 
 describe "API::Sleipnir::UserAPI" do
-  describe "GET /api/sleipnir/user/:id/profile.json" do
-    it "Trues" do
-      expect(true).to eq true
-    end
-  end
+
 end
