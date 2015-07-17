84f80c824ef9c3d434498cb842a77f54f0fb5b17
pixeldesu <git@pixelde.su>
2015-07-17 20:54:11 +0200

    fix broken pagination and wrong question count for deletion

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -5,6 +5,7 @@ class InboxController < ApplicationController
     @inbox = Inbox.where(user: current_user)
                   .order(:created_at).reverse_order
                   .paginate(page: params[:page])
+    @inbox_count = Inbox.where(user: current_user).count
     if params[:author].present?
       begin
         @author = true
@@ -12,15 +13,21 @@ class InboxController < ApplicationController
         @inbox_author = current_user.inboxes.joins(:question)
                                      .where(questions: { user_id: @target_user.id, author_is_anonymous: false })
                                      .paginate(page: params[:page])
+        @inbox_author_count = current_user.inboxes.joins(:question)
+                                                  .where(questions: { user_id: @target_user.id, author_is_anonymous: false })
+                                                  .count
         if @inbox_author.empty?
           flash[:info] = "No questions from @#{params[:author]} found, showing default entries instead!"
           @inbox
+          @inbox_count
         else
           @inbox = @inbox_author
+          @inbox_count = @inbox_author_count
         end
       rescue
         flash[:error] = "No user with the name @#{params[:author]} found, showing default entries instead!"
         @inbox
+        @inbox_count
       end
     end
     respond_to do |format|
