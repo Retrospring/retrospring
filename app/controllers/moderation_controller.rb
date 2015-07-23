deb6d121087765f3d3ad9cc4fac62c6670692276
Yuki <aaron.ahmed.eve@gmail.com>
2015-07-23 23:08:42 +0530

    Moderation Priority + IP

--- app/controllers/moderation_controller.rb
+++ app/controllers/moderation_controller.rb
@@ -9,4 +9,52 @@ class ModerationController < ApplicationController
                  Report.where(deleted: false).where('LOWER(type) = ?', "reports::#{@type}").reverse_order
                end
   end
+
+  def priority
+    @user_id = params[:user_id]
+    if @user_id.nil?
+      @users = {}
+      Report.where(deleted: false).each do |report|
+        target = if report.target.is_a? User
+          report.target
+        else
+          next if report.target.user.nil?
+          report.target.user
+        end
+
+        @users[target] ||= 0
+        @users[target] += 1
+      end
+
+      @users = @users.sort_by do |k, v|
+        v
+      end.reverse.to_h
+    else
+      @user_id = @user_id.to_i
+      @type = 'all'
+      @reports = []
+      Report.where(deleted: false).each do |report|
+        if report.target.is_a? User
+          @reports.push report if report.target.id == @user_id
+        else
+          next if report.target.user.nil?
+          @reports.push report if report.target.user.id == @user_id
+        end
+      end
+
+      @target_user = User.find(@user_id)
+      render template: 'moderation/index'
+    end
+  end
+
+  def ip
+    @user_id = params[:user_id]
+    @host = User.find(@user_id)
+    @users = []
+    return if @host.nil?
+    @users = User.where('(current_sign_in_ip = ? OR last_sign_in_ip = ?) AND id != ?', @host.current_sign_in_ip, @host.last_sign_in_ip, @user_id)
+    @users.unshift @host
+
+    render template: 'moderation/priority'
+  end
 end
