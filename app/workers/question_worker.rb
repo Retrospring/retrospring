97ad1285fe275c4dabe762a552814a9832b7b904
Yuki <aaron.ahmed.eve@gmail.com>
2015-07-27 13:16:26 +0530

    don't break loops

--- app/workers/question_worker.rb
+++ app/workers/question_worker.rb
@@ -11,11 +11,11 @@ class QuestionWorker
       user = User.find(user_id)
       if rcpt == 'followers'
         user.followers.each do |f|
-          Inbox.create!(user_id: f.id, question_id: question_id, new: true)
+          Inbox.create(user_id: f.id, question_id: question_id, new: true)
         end
       elsif rcpt.start_with? 'grp:'
         user.groups.find_by_name!(rcpt.sub 'grp:', '').members.each do |m|
-          Inbox.create!(user_id: m.user.id, question_id: question_id, new: true)
+          Inbox.create(user_id: m.user.id, question_id: question_id, new: true)
         end
       else
         logger.info "unknown rcpt #{rcpt}"
