c846b963a46ceaf14618aa642490f7fb5ffaada8
Yuki <aaron.ahmed.eve@gmail.com>
2015-07-27 13:12:00 +0530

    more fixes

--- app/workers/question_worker.rb
+++ app/workers/question_worker.rb
@@ -3,7 +3,7 @@ class QuestionWorker
 
   sidekiq_options queue: :question, retry: false
 
-  # @param rcpt [Integer] string recipient
+  # @param rcpt [String] recipient
   # @param user_id [Integer] user id passed from Devise
   # @param question_id [Integer] newly created question id
   def perform(rcpt, user_id, question_id)
@@ -11,12 +11,14 @@ class QuestionWorker
       user = User.find(user_id)
       if rcpt == 'followers'
         user.followers.each do |f|
-          Inbox.create(user_id: fid, question_id: question_id, new: true)
+          Inbox.create!(user_id: f.id, question_id: question_id, new: true)
         end
       elsif rcpt.start_with? 'grp:'
-        current_user.groups.find_by_name!(rcpt.sub 'grp:', '').members.each do |m|
-          Inbox.create(user_id: m.user.id, question_id: question.id, new: true)
+        user.groups.find_by_name!(rcpt.sub 'grp:', '').members.each do |m|
+          Inbox.create!(user_id: m.user.id, question_id: question_id, new: true)
         end
+      else
+        logger.info "unknown rcpt #{rcpt}"
       end
     rescue => e
       logger.info "failed to ask question: #{e.message}"
