89cede50ddc98f06d063eb603f691392229855e8
Andreas N <andy@pixelde.su>
2016-06-11 02:10:10 +0200

    update discover queries

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -4,15 +4,14 @@ class DiscoverController < ApplicationController
   def index
     top_x = 10  # only display the top X items
 
-    @popular_answers = Answer.where("created_at > ?", Time.now.ago(1.week)).order(:smile_count).reverse_order.limit(top_x)
-    @most_discussed = Answer.where("created_at > ?", Time.now.ago(1.week)).order(:comment_count).reverse_order.limit(top_x)
-    @popular_questions = Question.where("created_at > ?", Time.now.ago(1.week)).order(:answer_count).reverse_order.limit(top_x)
+    @popular_answers = Answer.all.order(:smile_count).reverse_order.limit(top_x)
+    @most_discussed = Answer.all.order(:comment_count).reverse_order.limit(top_x)
+    @popular_questions = Question.all.order(:answer_count).reverse_order.limit(top_x)
     @new_users = User.where("asked_count > 0").order(:id).reverse_order.limit(top_x)
 
     # .user = the user
     # .question_count = how many questions did the user ask
-    @users_with_most_questions = Question.select('user_id, COUNT(*) AS question_count').
-        where("created_at > ?", Time.now.ago(1.week)).
+    @users_with_most_questions = Question.all.select('user_id, COUNT(*) AS question_count').
         where(author_is_anonymous: false).
         group(:user_id).
         order('question_count').
@@ -20,8 +19,7 @@ class DiscoverController < ApplicationController
 
     # .user = the user
     # .answer_count = how many questions did the user answer
-    @users_with_most_answers = Answer.select('user_id, COUNT(*) AS answer_count').
-        where("created_at > ?", Time.now.ago(1.week)).
+    @users_with_most_answers = Answer.all.select('user_id, COUNT(*) AS answer_count').
         group(:user_id).
         order('answer_count').
         reverse_order.limit(top_x)
