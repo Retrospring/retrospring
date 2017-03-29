99bcfdacd49ee8c89f229f64a479a46f4d46e508
Georg Gadinger <nilsding@nilsding.org>
2017-03-29 22:06:43 +0200

    Add Exports to linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, "/usr/home/justask/apps/retrospring"
 append :linked_files, "config/database.yml", "config/justask.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/uploads", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/uploads", "public/system", "public/exports"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
