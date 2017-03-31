3a663eeb091c81758e37be8df83d2109ceb36117
Georg Gadinger <nilsding@nilsding.org>
2017-03-31 22:16:23 +0200

    App config with indifferent hash access

--- config/initializers/10_config.rb
+++ config/initializers/10_config.rb
@@ -1,5 +1,5 @@
 # Auxiliary config
-APP_CONFIG = YAML.load_file(Rails.root.join('config', 'justask.yml'))
+APP_CONFIG = YAML.load_file(Rails.root.join('config', 'justask.yml')).with_indifferent_access
 
 # Update rails config for mail
 Rails.application.config.action_mailer.default_url_options = { host: APP_CONFIG['hostname'] }
