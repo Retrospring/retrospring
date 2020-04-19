5eb61eb4183e103e79229cd3c81382098c7bd383
Georg Gadinger <nilsding@nilsding.org>
2020-04-19 15:34:45 +0200

    Use sendmail in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "justask_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # Use sendmail
+  config.action_mailer.delivery_method = :sendmail
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
