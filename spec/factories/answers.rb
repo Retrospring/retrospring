a0c9641994ef74969e86d451f888456e7b95b448
Georg Gadinger <nilsding@nilsding.org>
2020-04-19 21:47:19 +0200

    Remove useless specs

--- spec/factories/answers.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :answer do |u|
-    u.sequence(:content) { |n| "This is an answer.  I'm number #{n}!" }
-    u.user { FactoryBot.create(:user) }
-  end
-end
--- spec/factories/questions.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :question do |u|
-    u.sequence(:content) { |n| "#{QuestionGenerator.generate}#{n}" }
-    u.author_is_anonymous { true }
-  end
-end
