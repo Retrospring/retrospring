2ecc746e235d91168b56984ef16755e7e54eb738
Karina Kwiatek <dom@dom.dog>
2020-04-19 21:11:22 +0100

    Implement validation for announcements

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,3 +1,18 @@
 class Announcement < ApplicationRecord
   belongs_to :user
+
+  validates :content, presence: true
+  validates :starts_at, presence: true
+  validates :link_href, presence: true, if: -> { link_text.present? }
+  validate :starts_at, :validate_date_range
+
+  def link_present?
+    link_text.present?
+  end
+
+  def validate_date_range
+    if starts_at > ends_at
+      errors.add(:starts_at, "Start date must be before end date")
+    end
+  end
 end
