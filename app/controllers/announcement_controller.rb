e3b89f734626484e8d2552517e8c51f6b2c26fca
Karina Kwiatek <dom@dom.dog>
2020-04-19 20:34:48 +0100

    Implement creation of announcements

--- app/controllers/announcement_controller.rb
+++ app/controllers/announcement_controller.rb
@@ -4,9 +4,18 @@ class AnnouncementController < ApplicationController
   end
 
   def new
+    @announcement = Announcement.new
   end
 
   def create
+    @announcement = Announcement.new(announcement_params)
+    @announcement.user = current_user
+    if @announcement.save
+      flash[:success] = "Announcement created successfully."
+      redirect_to action: :index
+    else
+      render 'announcement/new'
+    end
   end
 
   def edit
@@ -16,5 +25,11 @@ class AnnouncementController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def announcement_params
+    params.require(:announcement).permit(:content, :link_text, :link_href, :starts_at, :ends_at)
   end
 end
