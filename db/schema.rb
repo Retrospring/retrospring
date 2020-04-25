10a9ca5577e99bc0520e6271f5d6c9a370d698f3
Karina Kwiatek <dom@dom.dog>
2020-04-25 21:01:46 +0100

    Fix bad replace; update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_185535) do
+ActiveRecord::Schema.define(version: 2020_04_25_194536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "announcements", force: :cascade do |t|
     t.text "content", null: false
@@ -241,12 +262,10 @@ ActiveRecord::Schema.define(version: 2020_04_19_185535) do
     t.integer "commented_count", default: 0, null: false
     t.string "display_name"
     t.integer "smiled_count", default: 0, null: false
-    t.boolean "admin", default: false, null: false
     t.string "motivation_header", default: "", null: false
     t.string "website", default: "", null: false
     t.string "location", default: "", null: false
     t.text "bio", default: "", null: false
-    t.boolean "moderator", default: false, null: false
     t.string "profile_picture_file_name"
     t.string "profile_picture_content_type"
     t.integer "profile_picture_file_size"
@@ -256,14 +275,11 @@ ActiveRecord::Schema.define(version: 2020_04_19_185535) do
     t.integer "crop_y"
     t.integer "crop_w"
     t.integer "crop_h"
-    t.boolean "supporter", default: false
     t.boolean "privacy_allow_anonymous_questions", default: true
     t.boolean "privacy_allow_public_timeline", default: true
     t.boolean "privacy_allow_stranger_answers", default: true
     t.boolean "privacy_show_in_search", default: true
     t.boolean "permanently_banned", default: false
-    t.boolean "blogger", default: false
-    t.boolean "contributor", default: false
     t.string "ban_reason"
     t.datetime "banned_until"
     t.integer "comment_smiled_count", default: 0, null: false
@@ -277,7 +293,6 @@ ActiveRecord::Schema.define(version: 2020_04_19_185535) do
     t.integer "crop_h_w"
     t.integer "crop_h_h"
     t.string "locale", default: "en"
-    t.boolean "translator", default: false
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
@@ -300,4 +315,5 @@ ActiveRecord::Schema.define(version: 2020_04_19_185535) do
     t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
