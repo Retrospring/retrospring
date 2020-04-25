8b8a5a1e7206957135536662fdbb52abbba7f921
Andreas Nedbal <andy@pixelde.su>
2020-04-25 17:02:59 +0200

    Add parent_layout helper

---
+++ app/helpers/layouts_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module LayoutsHelper
+  def parent_layout(layout)
+    @view_flow.set(:layout, output_buffer)
+    output = render(file: "layouts/#{layout}")
+    @haml_buffer.buffer.replace output
+    self.output_buffer = ActionView::OutputBuffer.new(output)
+  end
+end
