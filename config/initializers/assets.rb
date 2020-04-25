1aec0f95a41c5cdb523eb80a04043394a6f9ae4b
Andreas Nedbal <andy@pixelde.su>
2020-04-25 13:02:20 +0200

    Add configurable asset caching for Docker on Windows environments

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
+Rails.application.config.assets.configure do |env|
+  env.cache = Sprockets::Cache::FileStore.new(
+    ENV.fetch("SPROCKETS_CACHE", "#{env.root}/tmp/cache/assets"),
+    Rails.application.config.assets.cache_limit,
+    env.logger
+  )
+end
+
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
