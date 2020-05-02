3cc05a395fca09e4d56af297c869af7fefd697eb
Karina Kwiatek <dom@dom.dog>
2020-05-02 13:06:49 +0100

    Remove /system prefix from store_dir

--- app/uploaders/profile_header_uploader.rb
+++ app/uploaders/profile_header_uploader.rb
@@ -8,7 +8,7 @@ class ProfileHeaderUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "/system/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def paperclip_path
