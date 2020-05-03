241b8cbdcbb9cd3b1f18ad5bce3f992fc37c31b9
Andreas Nedbal <andy@pixelde.su>
2020-05-03 20:10:50 +0200

    Rename card variables to "raised"

--- app/helpers/theme_helper.rb
+++ app/helpers/theme_helper.rb
@@ -64,7 +64,7 @@ module ThemeHelper
       'info_text' => 'info-text',
       'success_color' => 'success',
       'success_text' => 'success-text',
-      'panel_color' => 'card-bg',
+      'panel_color' => 'raised-bg',
       'background_color' => 'background',
       'background_text' => 'body-text',
       'input_color' => 'input-bg',
