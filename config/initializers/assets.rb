d5d5c5eb1991d4f276bf19cc8b932c7a06ad9d04
Andreas Nedbal <andy@pixelde.su>
2020-05-06 14:31:27 +0200

    make sprockets cache definition development-only

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,11 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.assets.configure do |env|
-  env.cache = Sprockets::Cache::FileStore.new(
-    ENV.fetch("SPROCKETS_CACHE", "#{env.root}/tmp/cache/assets"),
-    Rails.application.config.assets.cache_limit,
-    env.logger
-  )
+unless Rails.env.production?
+  Rails.application.config.assets.configure do |env|
+    env.cache = Sprockets::Cache::FileStore.new(
+      ENV.fetch("SPROCKETS_CACHE", "#{env.root}/tmp/cache/assets"),
+      Rails.application.config.assets.cache_limit,
+      env.logger
+    )
+  end
 end
 
 # Version of your assets, change this if you want to expire all your assets.
