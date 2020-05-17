e8ba338f98be6d235da4d8708d03ef67363ba2a9
Andreas Nedbal <andy@pixelde.su>
2020-05-17 18:06:39 +0200

    Add registerEvents utility

---
+++ app/javascript/retrospring/utilities/registerEvents.ts
@@ -0,0 +1,17 @@
+interface EventDef {
+  type: string;
+  target: Node | NodeList;
+  handler: EventListenerOrEventListenerObject;
+}
+
+export default function registerEvents(events: EventDef[]): void {
+  events.forEach(event => {
+    if (event.target instanceof NodeList) {
+      event.target.forEach(element => {
+        element.addEventListener(event.type, event.handler);
+      });
+    } else {
+      event.target.addEventListener(event.type, event.handler);
+    }
+  });
+}
