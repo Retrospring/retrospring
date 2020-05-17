70aab0ffba97c0288adef428c19ef290b6db0f35
Andreas Nedbal <andy@pixelde.su>
2020-05-17 19:50:38 +0200

    Add initial implementation of question deletion functionality

--- app/javascript/retrospring/features/inbox.ts
+++ app/javascript/retrospring/features/inbox.ts
@@ -1,5 +1,7 @@
 import Rails from '@rails/ujs';
+import swal from 'sweetalert';
 
+import I18n from '../i18n';
 import registerEvents from '../utilities/registerEvents';
 
 export function generateQuestionHandler(): void {
@@ -15,9 +17,43 @@ export function generateQuestionHandler(): void {
     error: (data, status, xhr) => {
       console.log(data, status, xhr);
     }
+  });
+}
+
+export function deleteAllQuestionsHandler(event: Event): void {
+  const button = event.currentTarget as Element;
+  const count = button.getAttribute('data-ib-count');
+
+  swal({
+    title: I18n.t('frontend.inbox.confirm_all.title', { count: count }),
+    text: I18n.t('frontend.inbox.confirm_all.text'),
+    icon: 'warning',
+    dangerMode: true,
+    buttons: [
+      I18n.t('views.actions.cancel'),
+      I18n.t('views.actions.delete')
+    ]
   })
+  .then((returnValue) => {
+    if (returnValue === null) return false;
+    
+    Rails.ajax({
+      url: '/ajax/delete_all_inbox',
+      type: 'POST',
+      dataType: 'json',
+      success: (data) => {
+        if (!data.success) return false;
+  
+        document.querySelector('#entries').innerHTML = 'Nothing to see here!';
+      },
+      error: (data, status, xhr) => {
+        console.log(data, status, xhr);
+      }
+    });
+  });
 }
 
 export default (): void => registerEvents([
-  { type: 'click', target: document.querySelector('#ib-generate-question'), handler: generateQuestionHandler }
+  { type: 'click', target: document.querySelector('#ib-generate-question'), handler: generateQuestionHandler },
+  { type: 'click', target: document.querySelector('#ib-delete-all'), handler: deleteAllQuestionsHandler }
 ]);
