555ad78459757ed8f343f00f0982ef59933ef8b5
Andreas Nedbal <andy@pixelde.su>
2020-05-17 12:28:27 +0200

    Fix coding style issues

--- app/javascript/retrospring/initializers/bootstrap.js
+++ app/javascript/retrospring/initializers/bootstrap.js
@@ -3,10 +3,10 @@ import $ from 'jquery';
 
 /**
  * This module sets up Bootstraps JavaScript
- * 
+ *
  * Inside of the exported function below, initialize Bootstrap
  * modules that require explicit initilization, like tooltips
  */
-export default function() {
+export default function () {
   $(document).ready(() => $('[data-toggle="tooltip"]').tooltip());
 }
--- app/javascript/retrospring/initializers/index.js
+++ app/javascript/retrospring/initializers/index.js
@@ -2,7 +2,7 @@
  * Using some JavaScript trickery with require.context
  * and default exports, we're basically rebuilding the
  * Rails concept of "initializers" in JavaScript.
- * 
+ *
  * Every file in this folder exports a default function
  * which this index script is loading and executing, so
  * we don't have to specify several single import
@@ -11,7 +11,7 @@
  */
 export default function initialize() {
   const files = require.context('.', false, /\.js$/);
-  
+
   files.keys().forEach((key) => {
     if (key === './index.js') return;
     files(key).default();
