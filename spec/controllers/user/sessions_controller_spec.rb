febcf347ee03e60a1bc7bd94a7d3426ef8c9ca41
Dominik Kwiatek <dom@dom.dog>
2020-10-20 11:44:20 +0200

    Add basic login form tests

--- spec/controllers/user/sessions_controller_spec.rb
+++ spec/controllers/user/sessions_controller_spec.rb
@@ -5,8 +5,26 @@ describe User::SessionsController do
     @request.env["devise.mapping"] = Devise.mappings[:user]
   end
 
+  describe "#create" do
+    let(:user) { FactoryBot.create(:user, password: '/bin/animals64') }
+
+    subject { post :create, params: { user: { login: user.email, password: user.password } } }
+
+    it "logs in users without 2FA enabled without any further input" do
+      expect(subject).to redirect_to :root
+    end
+
+    it "prompts users with 2FA enabled to enter a code" do
+      user.otp_module = :enabled
+      user.save
+
+      expect(subject).to redirect_to :user_two_factor_entry
+    end
+  end
+
   describe "#two_factor_entry" do
     subject { get :two_factor_entry }
+
     it "redirects back to the home page if no sign in target is set" do
       expect(subject).to redirect_to :root
     end
