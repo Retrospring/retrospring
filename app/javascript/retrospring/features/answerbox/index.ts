aa9fc54ad2e750062cc30611d1bed121597725b7
Karina Kwiatek <kjk@kjk.dog>
2021-08-10 18:07:33 +0200

    Ensure answerboxes on profiles can properly attach sharing functionality

--- app/javascript/retrospring/features/answerbox/index.ts
+++ app/javascript/retrospring/features/answerbox/index.ts
@@ -7,9 +7,8 @@ export default (): void => {
     const entries: NodeList = document.querySelectorAll('.answerbox:not(.js-initialized)');
 
     entries.forEach((element: HTMLElement) => {
-      console.log(element)
       registerEvents([
-        { type: 'click', target: element.querySelector('[data-action=ab-share]'), handler: createShareEvent(element) }
+        { type: 'click', target: element.querySelector('[name=ab-share]'), handler: createShareEvent(element) }
       ]);
 
       element.classList.add('js-initialized');
--- app/javascript/retrospring/utilities/noop.ts
+++ app/javascript/retrospring/utilities/noop.ts
@@ -1,3 +1,3 @@
-export default () => void {
+export default (): void => {
   // do nothing
 }
