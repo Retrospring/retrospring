89127aa313da699225a7db9ba583b8c63a689537
Andreas Nedbal <git@pixelde.su>
2021-12-25 00:02:52 +0100

    Add aliases for common module locations

--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -1,6 +1,6 @@
-import start from '../retrospring/common';
-import initAnswerbox from '../retrospring/features/answerbox/index';
-import initInbox from '../retrospring/features/inbox/index';
+import start from 'retrospring/common';
+import initAnswerbox from 'retrospring/features/answerbox/index';
+import initInbox from 'retrospring/features/inbox/index';
 
 start();
 document.addEventListener('turbolinks:load', initAnswerbox);
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,16 @@
+const path = require('path')
 const { environment } = require('@rails/webpacker')
 const coffee =  require('./loaders/coffee')
 
 environment.loaders.prepend('coffee', coffee)
+
+environment.config.merge({
+  resolve: {
+    alias: {
+      retrospring: path.resolve(__dirname, '..', '..', 'app/javascript/retrospring'),
+      utilities: path.resolve(__dirname, '..', '..', 'app/javascript/retrospring/utilities')
+    }
+  }
+})
+
 module.exports = environment
