7f26c1fef31435fbd9ef4958ccd8b4575c2fe0f6
Karina Kwiatek <kjk@kjk.dog>
2021-12-25 22:08:06 +0100

    Ensure elipsis character always gets added for long questions/answers

--- app/models/services/twitter.rb
+++ app/models/services/twitter.rb
@@ -42,8 +42,8 @@ class Services::Twitter < Service
       tweet_text = ""
 
       until parsed_tweet[:valid]
-        tweet_text = "#{question_content[0..122]}#{'…' if original_question_length > question_content.length}" \
-          " — #{answer_content[0..123]}#{'…' if original_answer_length > answer_content.length} #{answer_url}"
+        tweet_text = "#{question_content[0..122]}#{'…' if original_question_length > [123, question_content.length].min}" \
+          " — #{answer_content[0..123]}#{'…' if original_answer_length > [124, answer_content.length].min} #{answer_url}"
 
         parsed_tweet = Twitter::TwitterText::Validation::parse_tweet(tweet_text)
 
