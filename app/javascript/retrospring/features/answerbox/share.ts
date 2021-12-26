453724be8ba9f4fbc53e06b72664d3ba134db0aa
Andreas Nedbal <git@pixelde.su>
2021-12-26 23:46:42 +0100

    Prevent reloading of page on share link click

--- app/javascript/retrospring/features/answerbox/share.ts
+++ app/javascript/retrospring/features/answerbox/share.ts
@@ -1,6 +1,7 @@
 import noop from 'utilities/noop';
 
 export function shareEventHandler(event: Event): void {
+  event.preventDefault();
   const answerbox = (event.target as HTMLElement).closest('.answerbox');
 
   navigator.share({
