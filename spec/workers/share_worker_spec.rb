d73e269d85e26a2b750406cd8b2384ee24ae5d72
Karina Kwiatek <kjk@kjk.dog>
2021-12-27 23:02:32 +0100

    Test `ShareWorker`'s handling of unhandled exceptions

--- spec/workers/share_worker_spec.rb
+++ spec/workers/share_worker_spec.rb
@@ -5,21 +5,29 @@ require 'rails_helper'
 describe ShareWorker do
   let(:user) { FactoryBot.create(:user) }
   let(:answer) { FactoryBot.create(:answer, user: user) }
+  let!(:service) { Services::Twitter.create!(type: 'Services::Twitter',
+                                                    user: user) }
 
   describe "#perform" do
-    subject { ShareWorker.new.perform(user.id, answer.id, 'twitter') }
-
-    before do
-      Service.create!(type: 'Services::Twitter',
-                      user: user)
-    end
+    subject {
+      Sidekiq::Testing.fake! do
+        ShareWorker.perform_async(user.id, answer.id, 'twitter')
+      end
+    }
 
     context 'when answer doesn\'t exist' do
       it 'prevents the job from retrying and logs a message' do
         answer.destroy!
         Sidekiq.logger.should_receive(:info)
-        subject
-        expect(ShareWorker.jobs.size).to eq(0)
+        expect { subject }.to change(ShareWorker.jobs, :size).by(1)
+        expect { ShareWorker.drain }.to change(ShareWorker.jobs, :size).by(-1)
+      end
+    end
+
+    context 'when answer exists' do
+      it 'retries on unhandled exceptions' do
+        expect { subject }.to change(ShareWorker.jobs, :size).by(1)
+        expect { ShareWorker.drain }.to raise_error(Twitter::Error::Forbidden)
       end
     end
   end
