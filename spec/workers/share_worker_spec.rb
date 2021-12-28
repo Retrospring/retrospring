a534dd04d55f2412b3106d2924b00782ddc55ad6
Karina Kwiatek <kjk@kjk.dog>
2021-12-28 03:59:45 +0100

    Test all cases of error handling for `ShareWorker`

--- spec/workers/share_worker_spec.rb
+++ spec/workers/share_worker_spec.rb
@@ -32,15 +32,30 @@ describe ShareWorker do
     context 'when answer doesn\'t exist' do
       it 'prevents the job from retrying and logs a message' do
         answer.destroy!
-        Sidekiq.logger.should_receive(:info)
+        Sidekiq.logger.should_receive(:info).with("Tried to post answer ##{answer.id} for user ##{user.id} to Twitter but the user/answer/service did not exist (likely deleted), will not retry.")
         expect { subject }.to change(ShareWorker.jobs, :size).by(1)
         expect { ShareWorker.drain }.to change(ShareWorker.jobs, :size).by(-1)
       end
     end
 
     context 'when answer exists' do
+      it 'handles Twitter::Error::DuplicateStatus' do
+        allow_any_instance_of(Services::Twitter).to receive(:post).with(answer).and_raise(Twitter::Error::DuplicateStatus)
+        Sidekiq.logger.should_receive(:info).with("Tried to post answer ##{answer.id} from user ##{user.id} to Twitter but the status was already posted.")
+        subject
+        ShareWorker.drain
+      end
+
+      it 'handles Twitter::Error::Unauthorized' do
+        allow_any_instance_of(Services::Twitter).to receive(:post).with(answer).and_raise(Twitter::Error::Unauthorized)
+        Sidekiq.logger.should_receive(:info).with("Tried to post answer ##{answer.id} from user ##{user.id} to Twitter but the token has exired or been revoked.")
+        subject
+        ShareWorker.drain
+      end
+
       it 'retries on unhandled exceptions' do
         expect { subject }.to change(ShareWorker.jobs, :size).by(1)
+        Sidekiq.logger.should_receive(:info)
         expect { ShareWorker.drain }.to raise_error(Twitter::Error::BadRequest)
       end
     end
