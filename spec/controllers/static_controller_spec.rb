968b2d88b5dc9d65f1c8c8acb1ba0d9215b0472f
Andreas Nedbal <git@pixelde.su>
2021-12-30 20:12:23 +0100

    Add tests for `StaticController#linkfilter`

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -18,4 +18,24 @@ describe StaticController, type: :controller do
       expect(assigns(:users)).to eq(1)
     end
   end
+
+  describe "#linkfilter" do
+    context "called without an url" do
+      subject { get :linkfilter }
+
+      it 'should redirect to the root page' do
+        subject
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "called with an url" do
+      subject { get :linkfilter, params: { url: 'https://google.com' } }
+
+      it 'should show the passed url' do
+        subject
+        expect(assigns(:link)).to eq('https://google.com')
+      end
+    end
+  end
 end
