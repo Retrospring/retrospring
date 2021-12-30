09172a56ef2702ad979d1bbb6433fd7b8f960534
Andreas Nedbal <git@pixelde.su>
2021-12-30 17:58:15 +0100

    Add linkfilter action

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,7 @@ Rails.application.routes.draw do
   match '/help/faq', to: 'static#faq', via: 'get', as: :help_faq
   match '/privacy', to: 'static#privacy_policy', via: 'get', as: :privacy_policy
   match '/terms', to: 'static#terms', via: 'get', as: :terms
+  match '/linkfilter', to: 'static#linkfilter', via: 'get', as: :linkfilter
 
   # Devise routes
   devise_for :users, path: 'user', skip: [:sessions, :registrations]
