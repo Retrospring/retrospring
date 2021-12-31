f871ab45e8f3ed3d28d3f44c019505d7995c5ed8
Andreas Nedbal <git@pixelde.su>
2021-12-31 22:23:45 +0100

    Allow `rel` in sanitized links

--- config/initializers/sanitize.rb
+++ config/initializers/sanitize.rb
@@ -1,7 +1,7 @@
 EVIL_TAGS = {
   elements: %w(blockquote a p i strong em del pre code table tr td th br ul ol li hr),
   attributes: {
-    'a' => %w(href target)
+    'a' => %w(href target rel)
   },
   protocols: {
     'a' => { 'href' => ['http', 'https', :relative] }
