fce693d3d3823256b2e703c935ac9c903849157e
Andreas Nedbal <andy@pixelde.su>
2022-01-02 19:26:04 +0100

    Apply review suggestion from @nilsding
    Co-authored-by: Georg Gadinger <nilsding@nilsding.org>

--- app/services/shared_markers.rb
+++ app/services/shared_markers.rb
@@ -3,11 +3,11 @@ module SharedMarkers
 
   def autolink(link, _link_type)
     href = if ALLOWED_HOSTS_IN_MARKDOWN.include?(URI(link).host)
-            link
-          else
-            linkfilter_path(url: link)
-          end
-    
+             link
+           else
+             linkfilter_path(url: link)
+           end
+
     content_tag(:a, link, href: href, target: "_blank", rel: "nofollow")
   rescue
     link
