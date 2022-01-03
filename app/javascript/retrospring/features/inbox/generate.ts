1a59f83f2ae70ebde457dbb076b50ae6ce071da7
Andreas Nedbal <git@pixelde.su>
2022-01-03 03:28:21 +0100

    Port inbox question generation functionality to TypeScript

---
+++ app/javascript/retrospring/features/inbox/generate.ts
@@ -0,0 +1,23 @@
+import Rails from '@rails/ujs';
+
+import I18n from '../../../legacy/i18n';
+import { updateDeleteButton } from './delete';
+import { showErrorNotification } from 'utilities/notifications';
+
+export function generateQuestionHandler(): void {
+  Rails.ajax({
+    url: '/ajax/generate_question',
+    type: 'POST',
+    dataType: 'json',
+    success: (data) => {
+      if (!data.success) return false;
+
+      document.querySelector('#entries').insertAdjacentHTML('afterbegin', data.render);
+      updateDeleteButton();
+    },
+    error: (data, status, xhr) => {
+      console.log(data, status, xhr);
+      showErrorNotification(I18n.t('frontend.error.message'));
+    }
+  });
+}
