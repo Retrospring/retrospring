8bd607f88caaea4032d508aa4a192c3831b97563
Andreas Nedbal <git@pixelde.su>
2022-01-03 03:30:14 +0100

    Export `on` method callback function type

--- app/javascript/retrospring/utilities/on.ts
+++ app/javascript/retrospring/utilities/on.ts
@@ -1,4 +1,4 @@
-type OnCallbackFunction = (event: Event) => void;
+export type OnCallbackFunction = (event: Event) => void;
 
 export function on(type: string, selector: string, callback: OnCallbackFunction): void {
   document.addEventListener(type, (event: Event) => {
