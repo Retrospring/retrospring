aef180277cbfb038eef4587108684faa7e1f6917
Andreas Nedbal <git@pixelde.su>
2022-01-03 03:32:54 +0100

    Extend `registerEvents` utility to accept global events

--- app/javascript/retrospring/utilities/registerEvents.ts
+++ app/javascript/retrospring/utilities/registerEvents.ts
@@ -1,17 +1,30 @@
+import { on, OnCallbackFunction } from './on';
+
 export interface EventDef {
     type: string;
-    target: Node | NodeList;
+    target: Node | NodeList | string;
     handler: EventListenerOrEventListenerObject;
+    global?: boolean;
 }
 
 export default function registerEvents(events: EventDef[]): void {
     events.forEach(event => {
-        if (event.target instanceof NodeList) {
-            event.target.forEach(element => {
-                element.addEventListener(event.type, event.handler);
+        if (event.global) {
+            if (typeof event.target === 'string') {
+                on(event.type, event.target, event.handler as OnCallbackFunction);
+            }
+        } else if (event.target instanceof NodeList) {
+            event.target.forEach((element: HTMLElement) => {
+                if (!element.classList.contains('js-initialized')) {
+                    element.addEventListener(event.type, event.handler);
+                    element.classList.add('js-initialized');
+                }
             });
         } else if (event.target instanceof Node) {
-            event.target.addEventListener(event.type, event.handler);
+            if (!(event.target as HTMLElement).classList.contains('js-initialized')) {
+                event.target.addEventListener(event.type, event.handler);
+                (event.target as HTMLElement).classList.add('js-initialized');
+            }
         }
     });
 }
