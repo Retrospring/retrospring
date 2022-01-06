f035cc2f895664c0d450fa2e7c6cc01433388ee9
Andreas Nedbal <git@pixelde.su>
2022-01-06 13:02:18 +0100

    Remove reference code comment

--- app/javascript/retrospring/features/settings/password.ts
+++ app/javascript/retrospring/features/settings/password.ts
@@ -1,11 +1,3 @@
-/*
-# see GitHub issue #11
-($ document).on "submit", "form#edit_user", (evt) ->
-  if ($ "input#user_current_password").val().length == 0
-    evt.preventDefault()
-    $("button[data-target=#modal-passwd]").trigger 'click'
-*/
-
 export function userSubmitHandler(event: Event): void {
   if (document.querySelector<HTMLInputElement>('#user_current_password').value.length === 0) {
     event.preventDefault();
