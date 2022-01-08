9ae4cfdc44c54ed82d6dae68fb7f09589571643c
Andreas Nedbal <git@pixelde.su>
2022-01-08 02:52:42 +0100

    Port report destroy functionality to TypeScript

---
+++ app/javascript/retrospring/features/moderation/destroy.ts
@@ -0,0 +1,39 @@
+import Rails from '@rails/ujs';
+import swal from 'sweetalert';
+
+import I18n from '../../../legacy/i18n';
+import { showNotification, showErrorNotification } from 'utilities/notifications';
+
+export function destroyReportHandler(event: Event): void {
+  const button = event.target as HTMLButtonElement;
+  const id = button.dataset.id;
+
+  swal({
+    title: I18n.translate('frontend.destroy_report.confirm.title'),
+    text: I18n.translate('frontend.destroy_report.confirm.text'),
+    type: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#DD6B55",
+    confirmButtonText: I18n.translate('views.actions.delete'),
+    cancelButtonText: I18n.translate('views.actions.cancel'),
+    closeOnConfirm: true
+  }, () => {
+    Rails.ajax({
+      url: '/ajax/mod/destroy_report',
+      type: 'POST',
+      data: new URLSearchParams({
+        id: id
+      }).toString(),
+      success: (data) => {
+        if (data.success) {
+          document.querySelector(`.moderationbox[data-id="${id}"]`).remove();
+        }
+        showNotification(data.message, data.success);
+      },
+      error: (data, status, xhr) => {
+        console.log(data, status, xhr);
+        showErrorNotification(I18n.translate('frontend.error.message'));
+      }
+    });
+  });
+}
