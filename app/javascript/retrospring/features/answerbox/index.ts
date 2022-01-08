747930a05442e14508b2ca2a0d29dd028000e2f4
Andreas Nedbal <git@pixelde.su>
2022-01-09 00:54:11 +0100

    Setup TypeScript answerbox feature

--- app/javascript/retrospring/features/answerbox/index.ts
+++ app/javascript/retrospring/features/answerbox/index.ts
@@ -1,10 +1,23 @@
-import { on } from 'retrospring/utilities/on';
+import registerEvents from 'utilities/registerEvents';
+import registerAnswerboxCommentEvents from './comment';
+import { answerboxDestroyHandler } from './destroy';
+import { answerboxReportHandler } from './report';
 import { shareEventHandler } from './share';
+import { answerboxSmileHandler } from './smile';
+import { answerboxSubscribeHandler } from './subscribe';
 
 export default (): void => {
   if ('share' in navigator) {
     document.body.classList.add('cap-web-share');
-
-    on('click', '[name=ab-share]', shareEventHandler);
   }
+
+  registerEvents([
+    { type: 'click', target: '[name=ab-share]', handler: shareEventHandler, global: true },
+    { type: 'click', target: '[data-action=ab-submarine]', handler: answerboxSubscribeHandler, global: true },
+    { type: 'click', target: '[data-action=ab-report]', handler: answerboxReportHandler, global: true },
+    { type: 'click', target: '[data-action=ab-destroy]', handler: answerboxDestroyHandler, global: true },
+    { type: 'click', target: '[name=ab-smile]', handler: answerboxSmileHandler, global: true }
+  ]);
+
+  registerAnswerboxCommentEvents();
 }
