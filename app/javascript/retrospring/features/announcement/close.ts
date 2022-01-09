486d2f4d91604f2bc689bb4b274ae247bbaebd5f
Karina Kwiatek <kjk@kjk.dog>
2022-01-09 18:16:36 +0100

    Address review feedback from @pixeldesu
    Co-authored-by: Andreas Nedbal <git@pixelde.su>

--- app/javascript/retrospring/features/announcement/close.ts
+++ app/javascript/retrospring/features/announcement/close.ts
@@ -1,5 +1,5 @@
 export default function (event: Event): void {
   const announcement = (event.target as HTMLElement).closest(".announcement") as HTMLDivElement;
-  const aId = announcement.dataset.announcementId;
-  window.localStorage.setItem(`announcement${aId}`, 'true');
+  const announcementId = announcement.dataset.announcementId;
+  window.localStorage.setItem(`announcement${announcementId}`, 'true');
 }
