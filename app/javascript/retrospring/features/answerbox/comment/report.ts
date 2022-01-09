46a5f24d07857c159aa3731cb57198821c7a6a23
Andreas Nedbal <git@pixelde.su>
2022-01-09 02:16:22 +0100

    Replace window report dialog usages with the TypeScript variant

--- app/javascript/retrospring/features/answerbox/comment/report.ts
+++ app/javascript/retrospring/features/answerbox/comment/report.ts
@@ -1,7 +1,9 @@
+import { reportDialog } from 'utilities/reportDialog';
+
 export function commentReportHandler(event: Event): void {
   event.preventDefault();
   const button = event.target as HTMLButtonElement;
   const commentId = button.dataset.cId;
 
-  window['reportDialog']('comment', commentId);
+  reportDialog('comment', commentId);
 }
--- app/javascript/retrospring/features/answerbox/report.ts
+++ app/javascript/retrospring/features/answerbox/report.ts
@@ -1,7 +1,9 @@
+import { reportDialog } from 'utilities/reportDialog';
+
 export function answerboxReportHandler(event: Event): void {
   event.preventDefault();
   const button = event.target as HTMLButtonElement;
   const answerId = button.dataset.aId;
 
-  window['reportDialog']('answer', answerId);
+  reportDialog('answer', answerId);
 }
