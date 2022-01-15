4a1ed5394a85ca87be7d06b11a4e9309858cf587
Karina Kwiatek <kjk@kjk.dog>
2022-01-15 12:38:10 +0100

    Remove relationships where source == target prior to adding relationship type

--- db/migrate/20211231155702_add_type_to_relationships.rb
+++ db/migrate/20211231155702_add_type_to_relationships.rb
@@ -2,6 +2,8 @@ class AddTypeToRelationships < ActiveRecord::Migration[5.2]
   def up
     add_column :relationships, :type, :string
 
+    execute %(delete from relationships where source_id = target_id)
+
     execute %(update relationships set type = 'Relationships::Follow')
 
     change_column_null :relationships, :type, false
