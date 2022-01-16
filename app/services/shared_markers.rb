486d097b54a94dbc581317a065ce994535f4aecf
Andreas Nedbal <git@pixelde.su>
2022-01-16 22:13:07 +0100

    QuestionMarkdown adjustments
    * Using `Redcarpet::Render::StripDown` as base now
    * Support for autolinks and named links in Markdown
    * Named links in questions just return the actual link as text
    * Fixed codestyle in files

--- app/services/shared_markers.rb
+++ app/services/shared_markers.rb
@@ -1,15 +1,34 @@
+# frozen_string_literal: true
+
 module SharedMarkers
   include ActionView::Helpers::TagHelper
 
-  def autolink(link, _link_type)
-    href = if ALLOWED_HOSTS_IN_MARKDOWN.include?(URI(link).host)
+  def process_link(link, text = nil)
+    href = if ALLOWED_HOSTS_IN_MARKDOWN.include?(URI(link).host) || URI(link).relative?
              link
            else
              linkfilter_path(url: link)
            end
 
-    content_tag(:a, link, href: href, target: "_blank", rel: "nofollow")
+    options = { href: href }
+
+    unless URI(link).relative?
+      options = options.merge({
+                                target: "_blank",
+                                rel:    "nofollow"
+                              })
+    end
+
+    content_tag(:a, text.nil? ? link : text, options)
   rescue
     link
+  end
+
+  def autolink(link, _link_type)
+    process_link(link)
+  end
+
+  def link(link, _title, content)
+    process_link(link, content)
   end
 end
