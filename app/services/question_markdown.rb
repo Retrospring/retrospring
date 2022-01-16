3bdca34c2d037beaf0569828edb7eb55b2b9c407
Andreas Nedbal <git@pixelde.su>
2022-01-16 22:13:07 +0100

    QuestionMarkdown adjustments
    * Using `Redcarpet::Render::StripDown` as base now
    * Support for autolinks and named links in Markdown
    * Named links in questions just return the actual link as text
    * Fixed codestyle in files

--- app/services/question_markdown.rb
+++ app/services/question_markdown.rb
@@ -1,10 +1,16 @@
-require 'uri'
+# frozen_string_literal: true
 
-class QuestionMarkdown < Redcarpet::Render::Base
+require "uri"
+
+class QuestionMarkdown < Redcarpet::Render::StripDown
   include Rails.application.routes.url_helpers
   include SharedMarkers
 
   def paragraph(text)
     "<p>#{text}</p>"
+  end
+
+  def link(link, _title, _content)
+    process_link(link)
   end
 end
