e3737bfb4875f75f89ffb4d6262d5698037da143
Andreas Nedbal <git@pixelde.su>
2022-01-22 08:16:55 +0100

    Fix Ruby codestyle

--- app/helpers/feedback_helper.rb
+++ app/helpers/feedback_helper.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 module FeedbackHelper
   def canny_token
     return if current_user.nil?
-    
-    userData = {
+
+    user_data = {
       avatarURL: current_user.profile_picture.url(:large),
-      name: current_user.screen_name,
-      id: current_user.id,
-      email: current_user.email
+      name:      current_user.screen_name,
+      id:        current_user.id,
+      email:     current_user.email
     }
 
-    JWT.encode(userData, APP_CONFIG.dig("canny", "sso"))
+    JWT.encode(user_data, APP_CONFIG.dig("canny", "sso"))
   end
 end
