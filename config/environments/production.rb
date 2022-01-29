800958e88f1dbabe1f1bde40a3232faa503c6258
Andreas Nedbal <git@pixelde.su>
2022-01-29 22:29:01 +0100

    Install and configure lograge

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,9 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
+  # Better log formatting
+  config.lograge.enabled = true
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
