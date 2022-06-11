26566de3e04a8306bdcef9c393e939f4d8f3abde
Karina Kwiatek <kjk@kjk.dog>
2022-06-11 23:10:20 +0200

    Only accept users to relationship use cases
    Dry Types was having issues with taking either an object or string so it's easier to deal with just passing in an object directly

--- lib/use_case/relationship/create.rb
+++ lib/use_case/relationship/create.rb
@@ -6,14 +6,11 @@ require "errors"
 module UseCase
   module Relationship
     class Create < UseCase::Base
-      option :source_user, type: Types::Strict::String | Types.Instance(::User)
-      option :target_user, type: Types::Strict::String | Types.Instance(::User)
+      option :source_user, type: Types.Instance(::User)
+      option :target_user, type: Types.Instance(::User)
       option :type, type: Types::RelationshipTypes
 
       def call
-        source_user = find_source_user
-        target_user = find_target_user
-
         source_user.public_send(type, target_user)
 
         {
@@ -23,24 +20,6 @@ module UseCase
             target_user: target_user
           }
         }
-      end
-
-      private
-
-      def find_source_user
-        return source_user if source_user.is_a?(::User)
-
-        ::User.find_by!(screen_name: source_user)
-      rescue ActiveRecord::RecordNotFound
-        raise Errors::UserNotFound
-      end
-
-      def find_target_user
-        return target_user if target_user.is_a?(::User)
-
-        ::User.find_by!(screen_name: target_user)
-      rescue ActiveRecord::RecordNotFound
-        raise Errors::UserNotFound
       end
     end
   end
