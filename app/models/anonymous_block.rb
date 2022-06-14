637fff657e2ce463d6dfc57ce58afe55ba12c9ac
Karina Kwiatek <kjk@kjk.dog>
2022-06-14 22:51:31 +0200

    Prevent anon questions from being sent while blocked

--- app/models/anonymous_block.rb
+++ app/models/anonymous_block.rb
@@ -1,4 +1,8 @@
 class AnonymousBlock < ApplicationRecord
   belongs_to :user
   belongs_to :question, optional: true
+
+  def self.get_identifier(ip)
+    Digest::SHA2.new(512).hexdigest(Rails.application.secret_key_base + ip)
+  end
 end
