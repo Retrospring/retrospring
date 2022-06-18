483c41379615d79351dd6b5fa34ea0c239bafbe2
Karina Kwiatek <kjk@kjk.dog>
2022-06-18 17:47:15 +0200

    Remove unused user relationship paginators

--- app/models/user/relationship_methods.rb
+++ app/models/user/relationship_methods.rb
@@ -3,18 +3,8 @@
 module User::RelationshipMethods
   include CursorPaginatable
 
-  define_cursor_paginator :cursored_followings, :ordered_followings
-  define_cursor_paginator :cursored_followers, :ordered_followers
   define_cursor_paginator :cursored_following_relationships, :ordered_following_relationships
   define_cursor_paginator :cursored_follower_relationships, :ordered_follower_relationships
-
-  def ordered_followings
-    followings.reverse_order.includes(:profile)
-  end
-
-  def ordered_followers
-    followers.reverse_order.includes(:profile)
-  end
 
   def ordered_following_relationships
     active_follow_relationships.reverse_order.includes(target: [:profile])
