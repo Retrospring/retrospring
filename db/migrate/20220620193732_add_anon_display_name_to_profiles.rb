442a75b0c8892dfc0365846509cdcc3d3f5f8a23
Karina Kwiatek <kjk@kjk.dog>
2022-06-21 14:50:51 +0200

    Add anon display name field to profiles

---
+++ db/migrate/20220620193732_add_anon_display_name_to_profiles.rb
@@ -0,0 +1,5 @@
+class AddAnonDisplayNameToProfiles < ActiveRecord::Migration[6.1]
+  def change
+    add_column :profiles, :anon_display_name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_14_141439) do
+ActiveRecord::Schema.define(version: 2022_06_20_193732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -147,6 +147,7 @@ ActiveRecord::Schema.define(version: 2022_06_14_141439) do
     t.string "motivation_header", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "anon_display_name"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
