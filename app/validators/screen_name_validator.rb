24c1719722e497eef0c3129bca6ffe4a71618fd8
Georg Gadinger <nilsding@nilsding.org>
2022-06-21 20:01:20 +0200

    screen_name_validator: use non-deprecated syntax for adding errors to fields

--- app/validators/screen_name_validator.rb
+++ app/validators/screen_name_validator.rb
@@ -8,7 +8,7 @@ class ScreenNameValidator < ActiveModel::EachValidator
   FORBIDDEN_SCREEN_NAME_REGEXPS = [/wreciap\z/i].freeze
 
   def validate_each(record, attribute, value)
-    record.errors[attribute] << "Thou shalt not use this username!  Please choose another one." if FORBIDDEN_SCREEN_NAMES.include? value.downcase
-    record.errors[attribute] << "Registration is tempoarily disabled for new users." if FORBIDDEN_SCREEN_NAME_REGEXPS.any? { |regexp| value.downcase =~ regexp }
+    record.errors.add(attribute, message: "Thou shalt not use this username!  Please choose another one.") if FORBIDDEN_SCREEN_NAMES.include?(value.downcase)
+    record.errors.add(attribute, message: "Registration is tempoarily disabled for new users.") if FORBIDDEN_SCREEN_NAME_REGEXPS.any? { |regexp| value.downcase =~ regexp }
   end
 end
