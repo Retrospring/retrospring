6bb1692bc5d45ebfc95bcc63556464931193fd80
Karina Kwiatek <kjk@kjk.dog>
2022-06-26 15:48:05 +0200

    Remove old ban fields from user table

---
+++ db/migrate/20220626134554_remove_ban_related_field_from_users.rb
@@ -0,0 +1,7 @@
+class RemoveBanRelatedFieldFromUsers < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :users, :permanently_banned, :boolean, default: false
+    remove_column :users, :ban_reason, :string, default: nil
+    remove_column :users, :banned_until, :datetime, default: nil
+  end
+end
