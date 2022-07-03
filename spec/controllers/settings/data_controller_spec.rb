f7a56fa58e5d147d8c6ad8d82a53aee119edede8
Andreas Nedbal <git@pixelde.su>
2022-07-03 21:43:04 +0200

    Add test for `Settings::DataController#index`

---
+++ spec/controllers/settings/data_controller_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Settings::DataController, type: :controller do
+  describe "#index" do
+    subject { get :index }
+
+    context "user signed in" do
+      let(:user) { FactoryBot.create(:user) }
+
+      before { sign_in user }
+
+      it "shows the index page" do
+        subject
+        expect(response).to have_rendered(:index)
+      end
+    end
+  end
+end
