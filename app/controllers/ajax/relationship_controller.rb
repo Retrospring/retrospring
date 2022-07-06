34de119bb1c55e6859700a9159b1c6223c7b8b51
Andreas Nedbal <git@pixelde.su>
2022-07-06 09:21:15 +0200

    Add translations for block/relationship ajax controllers

--- app/controllers/ajax/relationship_controller.rb
+++ app/controllers/ajax/relationship_controller.rb
@@ -16,7 +16,7 @@ class Ajax::RelationshipController < AjaxController
       type:        params[:type]
     )
     @response[:success] = true
-    @response[:message] = t("messages.#{params[:type]}.create.okay")
+    @response[:message] = t(".#{params[:type]}.success")
   rescue Errors::Base => e
     @response[:message] = t(e.locale_tag)
   ensure
@@ -30,7 +30,7 @@ class Ajax::RelationshipController < AjaxController
       type:        params[:type]
     )
     @response[:success] = true
-    @response[:message] = t("messages.#{params[:type]}.destroy.okay")
+    @response[:message] = t(".#{params[:type]}.success")
   rescue Errors::Base => e
     @response[:message] = t(e.locale_tag)
   ensure
