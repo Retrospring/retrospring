1b90f532c339e1479fef87d31d470ab7bbf9586b
Karina Kwiatek <kjk@kjk.dog>
2022-07-08 22:50:42 +0200

    Add migration to remove orphaned notifications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_08_203722) do
+ActiveRecord::Schema.define(version: 2022_07_08_204200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
