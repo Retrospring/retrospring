3b98117dfa9c078b2035b4ab3da37815f6274a2e
Karina Kwiatek <kjk@kjk.dog>
2022-07-16 15:55:09 +0200

    Update foreign keys instead of redefining them

--- db/migrate/20220714211614_set_on_delete_for_anonymous_blocks.rb
+++ db/migrate/20220714211614_set_on_delete_for_anonymous_blocks.rb
@@ -3,11 +3,8 @@
 class SetOnDeleteForAnonymousBlocks < ActiveRecord::Migration[6.1]
   def change
     change_table :anonymous_blocks do |t|
-      t.remove_references :user
-      t.references :user, null: false, foreign_key: true, on_delete: :cascade
-
-      t.remove_references :question
-      t.references :question, null: true, foreign_key: true, on_delete: :null
+      t.foreign_key :users, on_delete: :cascade
+      t.foreign_key :questions, on_delete: :cascade
     end
   end
 end
