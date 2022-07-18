c3eb216a4ce675bd4ddc340bb31404881d79772a
Karina Kwiatek <kjk@kjk.dog>
2022-07-18 16:51:03 +0200

    Move About, ToS & Privacy Policy into `AboutController`

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,41 +1,9 @@
 # frozen_string_literal: true
 
 class StaticController < ApplicationController
-  def about
-    user_count = User
-                 .where.not(confirmed_at: nil)
-                 .where("answered_count > 0")
-                 .count
-
-    current_ban_count = UserBan
-                        .current
-                        .joins(:user)
-                        .where.not("users.confirmed_at": nil)
-                        .where("users.answered_count > 0")
-                        .count
-
-    @users = user_count - current_ban_count
-    @questions = Question.count
-    @answers = Answer.count
-    @comments = Comment.count
-    @smiles = Appendable::Reaction.count
-  end
-
   def linkfilter
     redirect_to root_path unless params[:url]
     
     @link = params[:url]
-  end
-
-  def faq
-
-  end
-
-  def privacy_policy
-
-  end
-
-  def terms
-
   end
 end
--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -3,22 +3,6 @@
 require "rails_helper"
 
 describe StaticController, type: :controller do
-  describe "#about" do
-    subject { get :about }
-
-    before(:each) {
-      FactoryBot.create(:user, { confirmed_at: Time.current, answered_count: 1 })
-      FactoryBot.create(:user, { confirmed_at: Time.current, answered_count: 1 }).ban
-      FactoryBot.create(:user, { confirmed_at: Time.current })
-      FactoryBot.create(:user, { confirmed_at: Time.current }).ban
-    }
-
-    it "shows the correct user count" do
-      subject
-      expect(assigns(:users)).to eq(1)
-    end
-  end
-
   describe "#linkfilter" do
     context "called without an url" do
       subject { get :linkfilter }
