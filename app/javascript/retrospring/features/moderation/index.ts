3cabd39b89132afe9f2e7fd6f56877dc09f5e919
Andreas Nedbal <git@pixelde.su>
2022-07-18 22:29:48 +0200

    Remove vote functionality from TypeScript

--- app/javascript/retrospring/features/moderation/index.ts
+++ app/javascript/retrospring/features/moderation/index.ts
@@ -3,11 +3,9 @@ import registerCommentEvents from './comment';
 import { banCheckboxHandler, banFormHandler, permanentBanCheckboxHandler } from './ban';
 import { destroyReportHandler } from './destroy';
 import { privilegeCheckHandler } from './privilege';
-import { voteReportHandler } from './vote';
 
 export default (): void => {
   registerEvents([
-    { type: 'click', target: '[name=mod-vote]', handler: voteReportHandler, global: true },
     { type: 'click', target: '[type=checkbox][name=check-your-privileges]', handler: privilegeCheckHandler, global: true },
     { type: 'click', target: '[name=mod-delete-report]', handler: destroyReportHandler, global: true },
     { type: 'change', target: '[name="ban"][type="checkbox"]', handler: banCheckboxHandler, global: true },
