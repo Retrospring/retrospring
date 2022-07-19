2d34de5a57e5d75ecf27b14ed02c438604074bd8
Karina Kwiatek <kjk@kjk.dog>
2022-07-19 17:28:38 +0200

    Deduplicate shared logic in `TimelineController`

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -4,36 +4,30 @@ class TimelineController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @timeline = current_user.cursored_timeline(last_id: params[:last_id])
-    @timeline_last_id = @timeline.map(&:id).min
-    @more_data_available = !current_user.cursored_timeline(last_id: @timeline_last_id, size: 1).count.zero?
-
-    respond_to do |format|
-      format.html
-      format.js { render layout: false }
-    end
+    paginate_timeline { |args| current_user.cursored_timeline(**args) }
   end
 
   def list
     @list = current_user.lists.find_by!(name: params[:list_name])
-    @timeline = @list.cursored_timeline(last_id: params[:last_id])
-    @timeline_last_id = @timeline.map(&:id).min
-    @more_data_available = !@list.cursored_timeline(last_id: @timeline_last_id, size: 1).count.zero?
-
-    respond_to do |format|
-      format.html
-      format.js { render layout: false }
-    end
+    @title = list_title(current_user.lists.find_by!(name: params[:list_name]))
+    paginate_timeline { |args| @list.cursored_timeline(**args) }
   end
 
   def public
-    @timeline = Answer.cursored_public_timeline(last_id: params[:last_id])
+    @title = generate_title("Public Timeline")
+    paginate_timeline { |args| Answer.cursored_public_timeline(**args) }
+  end
+
+  private
+
+  def paginate_timeline
+    @timeline = yield(last_id: params[:last_id])
     @timeline_last_id = @timeline.map(&:id).min
-    @more_data_available = !Answer.cursored_public_timeline(last_id: @timeline_last_id, size: 1).count.zero?
+    @more_data_available = !yield(last_id: @timeline_last_id, size: 1).count.zero?
 
     respond_to do |format|
-      format.html
-      format.js { render layout: false }
+      format.html { render "timeline/timeline" }
+      format.js { render "timeline/timeline", layout: false }
     end
   end
 end
