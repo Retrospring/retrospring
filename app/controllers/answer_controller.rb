628959d7544486a64dfe9dc1af2c407a117940a9
Andreas Nedbal <git@pixelde.su>
2022-07-30 18:29:32 +0200

    Appease the reviewdog overlords

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class AnswerController < ApplicationController
   def show
-    @answer = Answer.includes(comments: [:user, :smiles], question: [:user], smiles: [:user]).find(params[:id])
+    @answer = Answer.includes(comments: %i[user smiles], question: [:user], smiles: [:user]).find(params[:id])
     @display_all = true
 
     if user_signed_in?
       notif = Notification.where(type: "Notification::QuestionAnswered", target_id: @answer.id, recipient_id: current_user.id, new: true).first
-      notif.update(new: false) unless notif.nil?
+      notif&.update(new: false)
       notif = Notification.where(type: "Notification::Commented", target_id: @answer.comments.pluck(:id), recipient_id: current_user.id, new: true)
       notif.update_all(new: false) unless notif.empty?
       notif = Notification.where(type: "Notification::Smiled", target_id: @answer.smiles.pluck(:id), recipient_id: current_user.id, new: true)
