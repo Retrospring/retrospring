a8e557d53625d00feb33ba5cb9be0058f51ad342
Andreas Nedbal <git@pixelde.su>
2022-08-17 23:19:47 +0200

    List direct questions for current users or mods

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -21,6 +21,10 @@ module UserHelper
     current_user&.mod? && session[:moderation_view] == true
   end
 
+  def belongs_to_current_user?
+    @user == current_user
+  end
+
   private
 
   def profile_link(user)
