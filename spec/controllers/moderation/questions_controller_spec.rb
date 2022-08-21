9874ffac19e16d8fbc308ab9c528ca9ddd74b1f5
Andreas Nedbal <git@pixelde.su>
2022-08-21 04:06:13 +0200

    Add base spec for `Moderation::QuestionsController`

---
+++ spec/controllers/moderation/questions_controller_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Moderation::QuestionsController, type: :controller do
+  let(:user) { FactoryBot.create :user, roles: ["moderator"] }
+
+  describe "#index" do
+    subject { get :index }
+
+    before do
+      sign_in user
+    end
+
+    it "renders the moderation/questions/index template" do
+      subject
+      expect(response).to render_template("moderation/questions/index")
+    end
+  end
+end
