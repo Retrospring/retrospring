4e65954a7ade6f8f7296ccc21ae412cd1223b0a2
Karina Kwiatek <kjk@kjk.dog>
2022-09-11 23:55:06 +0200

    Open inbox on notification click

--- public/service_worker.js
+++ public/service_worker.js
@@ -4,9 +4,19 @@ self.addEventListener('push', function (event) {
     console.log(event.data);
 
     event.waitUntil(self.registration.showNotification(notification.title, {
-      body: notification.body
+      body: notification.body,
+      tag: notification.type
     }));
   } else {
     console.error("Push event received, but it didn't contain any data.", event);
+  }
+});
+
+self.addEventListener('notificationclick', async event => {
+  if (event.notification.tag === 'inbox') {
+    event.preventDefault();
+    return clients.openWindow("/inbox", "_blank");
+  } else {
+    console.warn(`Unhandled notification tag: ${event.notification.tag}`);
   }
 });
