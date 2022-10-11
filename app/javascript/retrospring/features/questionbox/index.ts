4b8490a2d1ae97a87af8027a44d0d2fd09661af6
Georg Gadinger <nilsding@nilsding.org>
2022-10-11 22:08:59 +0200

    auto-foxus text field in ask modal
    resolves #557

--- app/javascript/retrospring/features/questionbox/index.ts
+++ app/javascript/retrospring/features/questionbox/index.ts
@@ -1,13 +1,17 @@
 import registerEvents from 'utilities/registerEvents';
-import { questionboxAllHandler, questionboxAllInputHandler } from './all';
+import { questionboxAllHandler, questionboxAllInputHandler, questionboxAllModalAutofocus } from './all';
 import { questionboxPromoteHandler, questionboxUserHandler, questionboxUserInputHandler } from './user';
 
 export default (): void => {
   registerEvents([
-    { type: 'click', target: '[name=qb-ask]', handler: questionboxUserHandler, global: true },
-    { type: 'click', target: '#new-question', handler: questionboxPromoteHandler, global: true },
-    { type: 'click', target: '[name=qb-all-ask]', handler: questionboxAllHandler, global: true },
-    { type: 'keydown', target: '[name=qb-question]', handler: questionboxUserInputHandler, global: true },
-    { type: 'keydown', target: '[name=qb-all-question]', handler: questionboxAllInputHandler, global: true }
+    { type: 'click', target: document.querySelectorAll('[name=qb-ask]'), handler: questionboxUserHandler },
+    { type: 'click', target: document.querySelector('#new-question'), handler: questionboxPromoteHandler },
+    { type: 'click', target: document.querySelectorAll('[name=qb-all-ask]'), handler: questionboxAllHandler },
+    { type: 'keydown', target: document.querySelectorAll('[name=qb-question]'), handler: questionboxUserInputHandler },
+    { type: 'keydown', target: document.querySelectorAll('[name=qb-all-question]'), handler: questionboxAllInputHandler }
   ]);
+
+  // unfortunately Bootstrap 4 relies on jQuery's event model, so I can't use registerEvents here :(
+  // TODO: when upgrading to Bootstrap 5 replace this with a normal DOM event
+  $('#modal-ask-followers').on('shown.bs.modal', questionboxAllModalAutofocus);
 }
