ee8fe0f4b813f3a73d13b36d32b0743955b2e3c3
Karina Kwiatek <kjk@kjk.dog>
2022-10-25 17:13:07 +0200

    Prevent comments from submitting after input as composition ends

--- app/javascript/retrospring/features/answerbox/comment/index.ts
+++ app/javascript/retrospring/features/answerbox/comment/index.ts
@@ -1,7 +1,7 @@
 import registerEvents from "retrospring/utilities/registerEvents"
 import { commentCharacterCountHandler } from "./count";
 import { commentDestroyHandler } from "./destroy";
-import { commentCreateHandler } from "./new";
+import {commentComposeEnd, commentComposeStart, commentCreateHandler} from "./new";
 import { commentReportHandler } from "./report";
 import { commentSmileHandler } from "./smile";
 import { commentToggleHandler } from "./toggle";
@@ -12,6 +12,8 @@ export default (): void => {
     { type: 'click', target: '[name=ab-smile-comment]', handler: commentSmileHandler, global: true },
     { type: 'click', target: '[data-action=ab-comment-report]', handler: commentReportHandler, global: true },
     { type: 'click', target: '[data-action=ab-comment-destroy]', handler: commentDestroyHandler, global: true },
+    { type: 'compositionstart', target: '[name=ab-comment-new]', handler: commentComposeStart, global: true },
+    { type: 'compositionend', target: '[name=ab-comment-new]', handler: commentComposeEnd, global: true },
     { type: 'keyup', target: '[name=ab-comment-new]', handler: commentCreateHandler, global: true },
     { type: 'input', target: '[name=ab-comment-new]', handler: commentCharacterCountHandler, global: true }
   ]);
