0b499138e0e9cbfc7305153655cd631140494299
Andreas Nedbal <git@pixelde.su>
2022-11-15 21:34:03 +0100

    Add social graph hiding field to user

---
+++ db/migrate/20221115194933_add_privacy_hide_social_graph.rb
@@ -0,0 +1,9 @@
+class AddPrivacyHideSocialGraph < ActiveRecord::Migration[6.1]
+  def up
+    add_column :users, :privacy_hide_social_graph, :boolean, default: false
+  end
+
+  def down
+    remove_column :users, :privacy_hide_social_graph
+  end
+end
