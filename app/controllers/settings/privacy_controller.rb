276268efa9ce21fc3e5b8141406ccba8203d2fc7
Andreas Nedbal <git@pixelde.su>
2022-11-15 21:34:45 +0100

    Add social graph field to privacy settings view

--- app/controllers/settings/privacy_controller.rb
+++ app/controllers/settings/privacy_controller.rb
@@ -11,7 +11,8 @@ class Settings::PrivacyController < ApplicationController
                                                    :privacy_allow_public_timeline,
                                                    :privacy_allow_stranger_answers,
                                                    :privacy_show_in_search,
-                                                   :privacy_require_user)
+                                                   :privacy_require_user,
+                                                   :privacy_hide_social_graph)
     if current_user.update(user_attributes)
       flash[:success] = t(".success")
     else
