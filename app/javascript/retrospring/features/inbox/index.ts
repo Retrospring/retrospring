0b5ebf42c144f62420c3032342d5b3723ff3b9c4
Andreas Nedbal <git@pixelde.su>
2022-11-17 21:56:23 +0100

    Remove AJAX question generation code

--- app/javascript/retrospring/features/inbox/index.ts
+++ app/javascript/retrospring/features/inbox/index.ts
@@ -2,11 +2,9 @@ import registerEvents from 'utilities/registerEvents';
 import registerInboxEntryEvents from './entry';
 import { authorSearchHandler } from './author';
 import { deleteAllAuthorQuestionsHandler, deleteAllQuestionsHandler } from './delete';
-import { generateQuestionHandler } from './generate';
 
 export default (): void => {
   registerEvents([
-    { type: 'click', target: '#ib-generate-question', handler: generateQuestionHandler, global: true },
     { type: 'click', target: '#ib-delete-all', handler: deleteAllQuestionsHandler, global: true },
     { type: 'click', target: '#ib-delete-all-author', handler: deleteAllAuthorQuestionsHandler, global: true },
     { type: 'submit', target: '#author-form', handler: authorSearchHandler, global: true }
