41c43538b5aba32d88709bbe52b23c08e582aabc
Andreas Nedbal <git@pixelde.su>
2022-11-20 16:38:02 +0100

    Fix lints

--- app/models/mute_rule.rb
+++ app/models/mute_rule.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class MuteRule < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of :muted_phrase
+  validates :muted_phrase, presence: true
 
   def applies_to?(post)
     !!(post.content =~ /\b#{Regexp.escape(muted_phrase)}\b/i)
