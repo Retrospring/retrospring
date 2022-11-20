40d5dc41dd57a086268069b9407048a00f85a563
Karina Kwiatek <kjk@kjk.dog>
2022-11-20 15:06:20 +0100

    Ensure mute rules cannot be empty

--- app/models/mute_rule.rb
+++ app/models/mute_rule.rb
@@ -1,6 +1,8 @@
 class MuteRule < ApplicationRecord
   belongs_to :user
 
+  validates_presence_of :muted_phrase
+
   def applies_to?(post)
     !!(post.content =~ /\b#{Regexp.escape(muted_phrase)}\b/i)
   end
