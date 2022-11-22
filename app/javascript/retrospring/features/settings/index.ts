55f26cb7d445639ec3c8d415d5b09668e648ef48
Andreas Nedbal <git@pixelde.su>
2022-11-22 08:38:33 +0100

    Remove settings unblock TypeScript

--- app/javascript/retrospring/features/settings/index.ts
+++ app/javascript/retrospring/features/settings/index.ts
@@ -2,7 +2,6 @@ import registerEvents from "utilities/registerEvents";
 import { profileHeaderChangeHandler, profilePictureChangeHandler } from "./crop";
 import { themeDocumentHandler, themeSubmitHandler } from "./theme";
 import { userSubmitHandler } from "./password";
-import { unblockAnonymousHandler } from "./block";
 
 export default (): void => {
   themeDocumentHandler();
@@ -11,7 +10,6 @@ export default (): void => {
     { type: 'submit', target: document.querySelector('form.edit_theme, form.new_theme'), handler: themeSubmitHandler },
     { type: 'submit', target: document.querySelector('#edit_user'), handler: userSubmitHandler },
     { type: 'change', target: document.querySelector('#user_profile_picture[type=file]'), handler: profilePictureChangeHandler },
-    { type: 'change', target: document.querySelector('#user_profile_header[type=file]'), handler: profileHeaderChangeHandler },
-    { type: 'click', target: document.querySelectorAll('[data-action="anon-unblock"]'), handler: unblockAnonymousHandler }
+    { type: 'change', target: document.querySelector('#user_profile_header[type=file]'), handler: profileHeaderChangeHandler }
   ]);
 }
