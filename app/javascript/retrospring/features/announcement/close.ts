b51d8c048d8d7144d8cf07554462608ce3fe9821
Andreas Nedbal <git@pixelde.su>
2022-11-23 21:01:35 +0100

    Remove legacy announcement TypeScript

--- app/javascript/retrospring/features/announcement/close.ts
+++
@@ -1,5 +0,0 @@
-export default function (event: Event): void {
-  const announcement = (event.target as HTMLElement).closest(".announcement") as HTMLDivElement;
-  const announcementId = announcement.dataset.announcementId;
-  window.localStorage.setItem(`announcement${announcementId}`, 'true');
-}
