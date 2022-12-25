91d3db4034b40ebe5b0523bb8e88e772e092040c
Karina Kwiatek <kjk@kjk.dog>
2022-12-25 23:34:04 +0000

    Move rpush init/exit logic into Sidekiq initializer
    Co-authored-by: nilsding <nilsding@nilsding.org>

--- app/workers/push_notification_worker.rb
+++ app/workers/push_notification_worker.rb
@@ -8,10 +8,6 @@ class PushNotificationWorker
   sidekiq_options queue: :push_notification, retry: 0
 
   def perform(notification_id)
-    Rpush.config.push = true
-    Rpush::Daemon.common_init
-    Rpush::Daemon::Synchronizer.sync
     Rpush::Daemon::AppRunner.enqueue(Rpush::Client::ActiveRecord::Notification.where(id: notification_id))
-    Rpush::Daemon::AppRunner.stop
   end
 end
