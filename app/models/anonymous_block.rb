e7f60acad5f0c8f8a8beaa692626664193e72d12
Andreas Nedbal <git@pixelde.su>
2022-12-27 02:26:06 +0100

    Add target_user field to anonymous blocks

--- app/models/anonymous_block.rb
+++ app/models/anonymous_block.rb
@@ -5,6 +5,7 @@ require "digest"
 class AnonymousBlock < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :question, optional: true
+  belongs_to :target_user, class_name: "User", optional: true
 
   def self.get_identifier(ip)
     Digest::SHA2.new(512).hexdigest(Rails.application.secret_key_base + ip)
