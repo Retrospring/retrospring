f80d4ce9351a308c20ea5b6971250e399f391c92
Karina Kwiatek <kjk@kjk.dog>
2023-01-02 09:15:51 +0100

    Move create test into a describe block

--- spec/controllers/services_controller_spec.rb
+++ spec/controllers/services_controller_spec.rb
@@ -3,45 +3,47 @@
 require "rails_helper"
 
 describe ServicesController, type: :controller do
-  context "successful Twitter sign in" do
-    let(:user) { FactoryBot.create(:user) }
-
-    before do
-      sign_in user
-      OmniAuth.config.test_mode = true
-      OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
-                                                                     "provider"    => "twitter",
-                                                                     "uid"         => "12",
-                                                                     "info"        => { "nickname" => "jack" },
-                                                                     "credentials" => { "token" => "AAAA", "secret" => "BBBB" }
-                                                                   })
-      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
-    end
-
-    after do
-      OmniAuth.config.mock_auth[:twitter] = nil
-    end
-
+  describe "#create" do
     subject { get :create, params: { provider: "twitter" } }
 
-    context "no services connected" do
-      it "creates a service integration" do
-        expect { subject }.to change { Service.count }.by(1)
+    context "successful Twitter sign in" do
+      let(:user) { FactoryBot.create(:user) }
+
+      before do
+        sign_in user
+        OmniAuth.config.test_mode = true
+        OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+                                                                       "provider"    => "twitter",
+                                                                       "uid"         => "12",
+                                                                       "info"        => { "nickname" => "jack" },
+                                                                       "credentials" => { "token" => "AAAA", "secret" => "BBBB" }
+                                                                     })
+        request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
       end
-    end
 
-    context "a user has a service connected" do
-      let(:other_user) { FactoryBot.create(:user) }
-      let!(:service) { Services::Twitter.create(user: other_user, uid: 12) }
+      after do
+        OmniAuth.config.mock_auth[:twitter] = nil
+      end
 
-      it "shows an error when trying to attach a service account which is already connected" do
-        subject
-        expect(flash[:error]).to eq("The Twitter account you are trying to connect is already connected to another #{APP_CONFIG['site_name']} account. If you are unable to disconnect the account yourself, please send us a Direct Message on Twitter: @retrospring.")
+      context "no services connected" do
+        it "creates a service integration" do
+          expect { subject }.to change { Service.count }.by(1)
+        end
+      end
+
+      context "a user has a service connected" do
+        let(:other_user) { FactoryBot.create(:user) }
+        let!(:service) { Services::Twitter.create(user: other_user, uid: 12) }
+
+        it "shows an error when trying to attach a service account which is already connected" do
+          subject
+          expect(flash[:error]).to eq("The Twitter account you are trying to connect is already connected to another #{APP_CONFIG['site_name']} account. If you are unable to disconnect the account yourself, please send us a Direct Message on Twitter: @retrospring.")
+        end
       end
     end
   end
 
-  context "#update" do
+  describe "#update" do
     subject { patch :update, params: }
 
     context "not signed in" do
