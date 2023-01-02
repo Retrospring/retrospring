1adf3956ba370ea055e377f1fac967010bc544d2
Karina Kwiatek <kjk@kjk.dog>
2023-01-02 12:57:14 +0100

    Remove `console.log`

--- public/service_worker.js
+++ public/service_worker.js
@@ -1,7 +1,6 @@
 self.addEventListener('push', function (event) {
   if (event.data) {
     const notification = event.data.json();
-    console.log(event.data);
 
     event.waitUntil(self.registration.showNotification(notification.title, {
       body: notification.body,
