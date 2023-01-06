9edadfdda0ea7953c50ed95faa0fece0eafe8da8
Andreas Nedbal <git@pixelde.su>
2023-01-06 08:55:50 +0100

    Remove Webpacker

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.asset_source = :webpacker
+  config.asset_source = :sprockets
 
   # Code is not reloaded between requests.
   config.cache_classes = true
