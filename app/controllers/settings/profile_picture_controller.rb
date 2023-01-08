8ba2df1b80519cb65b9c38feceb65cbebb878875
Karina Kwiatek <kjk@kjk.dog>
2023-01-08 20:51:19 +0100

    Streamline strings for profile picture settings controller

--- app/controllers/settings/profile_picture_controller.rb
+++ app/controllers/settings/profile_picture_controller.rb
@@ -7,14 +7,26 @@ class Settings::ProfilePictureController < ApplicationController
     user_attributes = params.require(:user).permit(:show_foreign_themes, :profile_picture_x, :profile_picture_y, :profile_picture_w, :profile_picture_h,
                                                    :profile_header_x, :profile_header_y, :profile_header_w, :profile_header_h, :profile_picture, :profile_header)
     if current_user.update(user_attributes)
-      text = t(".success")
-      text += t(".notice.profile_picture") if user_attributes[:profile_picture]
-      text += t(".notice.profile_header") if user_attributes[:profile_header]
-      flash[:success] = text
+      flash[:success] = success_flash_message(user_attributes)
     else
       flash[:error] = t(".error")
     end
 
     redirect_to settings_profile_path
+  end
+
+  private
+
+  def success_flash_message(user_attributes)
+    if user_attributes[:profile_picture] && user_attributes[:profile_header]
+      t(".update.success.both")
+    elsif user_attributes[:profile_picture]
+      t(".update.success.profile_picture")
+    elsif user_attributes[:profile_header]
+      t(".update.success.profile_header")
+    else
+      state = user_attributes[:show_foreign_themes] ? "enabled" : "disabled"
+      t(".update.success.foreign_themes.#{state}")
+    end
   end
 end
