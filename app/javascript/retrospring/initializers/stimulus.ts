5f529b2b262cbf0d6ab4122a373328ed4ed68bfa
Andreas Nedbal <git@pixelde.su>
2023-01-17 07:39:14 +0100

    Add Stimulus theme controller

--- app/javascript/retrospring/initializers/stimulus.ts
+++ app/javascript/retrospring/initializers/stimulus.ts
@@ -5,6 +5,7 @@ import CharacterCountController from "retrospring/controllers/character_count_co
 import CharacterCountWarningController from "retrospring/controllers/character_count_warning_controller";
 import FormatPopupController from "retrospring/controllers/format_popup_controller";
 import CollapseController from "retrospring/controllers/collapse_controller";
+import ThemeController from "retrospring/controllers/theme_controller";
 
 /**
  * This module sets up Stimulus and our controllers
@@ -21,4 +22,5 @@ export default function (): void {
   window['Stimulus'].register('character-count-warning', CharacterCountWarningController);
   window['Stimulus'].register('collapse', CollapseController);
   window['Stimulus'].register('format-popup', FormatPopupController);
+  window['Stimulus'].register('theme', ThemeController);
 }
