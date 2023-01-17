79fa204897235c6484dc09430a76ce93a4f0fe54
Andreas Nedbal <git@pixelde.su>
2023-01-17 07:38:02 +0100

    Remove theming handler features

--- app/javascript/retrospring/features/settings/index.ts
+++ app/javascript/retrospring/features/settings/index.ts
@@ -1,13 +1,9 @@
 import registerEvents from "utilities/registerEvents";
 import { profileHeaderChangeHandler, profilePictureChangeHandler } from "./crop";
-import { themeDocumentHandler, themeSubmitHandler } from "./theme";
 import { userSubmitHandler } from "./password";
 
 export default (): void => {
-  themeDocumentHandler();
-
   registerEvents([
-    { type: 'submit', target: document.querySelector('form.edit_theme, form.new_theme'), handler: themeSubmitHandler },
     { type: 'submit', target: document.querySelector('#edit_user'), handler: userSubmitHandler },
     { type: 'change', target: document.querySelector('#user_profile_picture[type=file]'), handler: profilePictureChangeHandler },
     { type: 'change', target: document.querySelector('#user_profile_header[type=file]'), handler: profileHeaderChangeHandler }
