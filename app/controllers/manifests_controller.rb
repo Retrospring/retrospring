b9269f0693aa514c97e15402301c44006aff1068
Karina Kwiatek <kjk@kjk.dog>
2023-01-21 18:16:24 +0100

    Add maskable icons

--- app/controllers/manifests_controller.rb
+++ app/controllers/manifests_controller.rb
@@ -39,8 +39,10 @@ class ManifestsController < ApplicationController
   def webapp_icons
     %i[1024 512 384 192 128 96 72 48].map do |size|
       [
-        { src: "/icons/icon_x#{size}.webp", sizes: "#{size}x#{size}", type: "image/webp" },
-        { src: "/icons/icon_x#{size}.png", sizes: "#{size}x#{size}", type: "image/png" }
+        { src: "/icons/icon_x#{size}.webp", sizes: "#{size}x#{size}", type: "image/webp", purpose: "any" },
+        { src: "/icons/icon_x#{size}.png", sizes: "#{size}x#{size}", type: "image/png", purpose: "any" },
+        { src: "/icons/maskable_icon_x#{size}.webp", sizes: "#{size}x#{size}", type: "image/webp", purpose: "maskable" },
+        { src: "/icons/maskable_icon_x#{size}.png", sizes: "#{size}x#{size}", type: "image/png", purpose: "maskable" }
       ]
     end.flatten
   end
