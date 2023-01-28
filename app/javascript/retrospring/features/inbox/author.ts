8dd49d3a8c1c0b4c408f5530a80fa0c8ef8e0fd7
Andreas Nedbal <git@pixelde.su>
2023-01-28 19:17:04 +0100

    Remove author search handler

--- app/javascript/retrospring/features/inbox/author.ts
+++
@@ -1,6 +0,0 @@
-export function authorSearchHandler(event: Event): void {
-  event.preventDefault();
-
-  const author = document.querySelector<HTMLInputElement>('#author')?.value;
-  window.location.href = `/inbox/${encodeURIComponent(author)}`;
-}
