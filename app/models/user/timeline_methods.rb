32ba17ac72f2971a3470dfc16f88f22fb3b8b2f2
Karina Kwiatek <kjk@kjk.dog>
2023-01-28 21:17:08 +0100

    Eager load profiles in paginators

--- app/models/user/timeline_methods.rb
+++ app/models/user/timeline_methods.rb
@@ -5,8 +5,12 @@ module User::TimelineMethods
 
   define_cursor_paginator :cursored_timeline, :timeline
 
-  # @return [Array] the users' timeline
+  # @return [ActiveRecord::Relation<Answer>] the user's timeline
   def timeline
-    Answer.where("user_id in (?) OR user_id = ?", following_ids, id).order(:created_at).reverse_order.includes(comments: %i[user smiles], question: [:user], user: [:profile], smiles: [:user])
+    Answer
+      .where("user_id in (?) OR user_id = ?", following_ids, id)
+      .order(:created_at)
+      .reverse_order
+      .includes(comments: %i[user smiles], question: { user: :profile }, user: [:profile], smiles: [:user])
   end
 end
