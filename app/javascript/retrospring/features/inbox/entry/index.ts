83d386267a861e69d6bcaf873dcafe8193cace8e
Andreas Nedbal <git@pixelde.su>
2023-02-05 18:17:12 +0100

    Remove inbox entry options collapse

--- app/javascript/retrospring/features/inbox/entry/index.ts
+++ app/javascript/retrospring/features/inbox/entry/index.ts
@@ -1,7 +1,6 @@
 import registerEvents from 'utilities/registerEvents';
 import { answerEntryHandler, answerEntryInputHandler } from './answer';
 import { deleteEntryHandler } from './delete';
-import optionsEntryHandler from './options';
 import { reportEventHandler } from './report';
 
 export default (): void => {
@@ -9,7 +8,6 @@ export default (): void => {
     { type: 'click', target: 'button[name="ib-answer"]', handler: answerEntryHandler, global: true },
     { type: 'click', target: '[name="ib-destroy"]', handler: deleteEntryHandler, global: true },
     { type: 'click', target: '[name=ib-report]', handler: reportEventHandler, global: true },
-    { type: 'click', target: 'button[name=ib-options]', handler: optionsEntryHandler, global: true },
     { type: 'keydown', target: 'textarea[name=ib-answer]', handler: answerEntryInputHandler, global: true }
   ]);
 }
