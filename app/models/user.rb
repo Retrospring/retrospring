d520755bc2e0f00ae34d8a1d9db8e286cbe8f903
Andreas Nedbal <git@pixelde.su>
2023-02-05 18:03:52 +0100

    Remove service model

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,6 @@ class User < ApplicationRecord # rubocop:disable Metrics/ClassLength
   has_many :comments, dependent: :destroy_async
   has_many :inboxes, dependent: :destroy_async
   has_many :smiles, class_name: "Appendable::Reaction", dependent: :destroy_async
-  has_many :services, dependent: :destroy_async
   has_many :notifications, foreign_key: :recipient_id, dependent: :destroy_async
   has_many :reports, dependent: :destroy_async
   has_many :lists, dependent: :destroy_async
