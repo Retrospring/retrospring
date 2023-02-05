024127e62fdff78f638a3864624c9439cf56605d
Andreas Nedbal <git@pixelde.su>
2023-02-05 21:13:37 +0100

    Remove connected user transformation from TwitteredMarkdown

--- app/services/twittered_markdown.rb
+++ app/services/twittered_markdown.rb
@@ -7,17 +7,7 @@ class TwitteredMarkdown < Redcarpet::Render::StripDown
 
   def wrap_mentions(text)
     text.gsub(/(^|\s)@([a-zA-Z0-9_]{1,16})/) do
-      local_user = User.find_by(screen_name: $2)
-      if local_user.nil?
-        "#{$1}#{$2}"
-      else
-        service = local_user.services.where(type: "Services::Twitter").first
-        if service.nil?
-          "#{$1}#{$2}"
-        else
-          "#{$1}@#{service.nickname}"
-        end
-      end
+      "#{$1}#{$2}"
     end
   end
 end
