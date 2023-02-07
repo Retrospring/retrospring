1a2650e8107b4d18a2146792a49291bae7e0fbdd
Karina Kwiatek <kjk@kjk.dog>
2023-02-07 23:57:36 +0100

    Add `@github/hotkey`

--- app/javascript/retrospring/common.ts
+++ app/javascript/retrospring/common.ts
@@ -1,11 +1,13 @@
 import '@hotwired/turbo-rails';
 import initializeBootstrap from './initializers/bootstrap';
+import initializeHotkey from './initializers/hotkey';
 import initializeServiceWorker from './initializers/serviceWorker';
 import initializeStimulus from './initializers/stimulus';
 
 export default function start(): void {
   try {
     initializeBootstrap();
+    initializeHotkey();
     initializeServiceWorker();
     initializeStimulus();
   } catch (e) {
