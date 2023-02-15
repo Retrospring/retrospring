e85aaadb63cd980b427b444d21c21c56a5142dc9
Karina Kwiatek <kjk@kjk.dog>
2023-02-15 23:50:48 +0100

    Pass Markdown renderer options into renderer

--- config/initializers/redcarpet.rb
+++ config/initializers/redcarpet.rb
@@ -1,32 +1,37 @@
-require 'redcarpet/render_strip'
+# frozen_string_literal: true
+
+require "redcarpet/render_strip"
 
 MARKDOWN_OPTS = {
-    filter_html: true,
-    escape_html: true,
-    no_images: true,
-    no_styles: true,
-    safe_links_only: true,
-    xhtml: false,
-    hard_wrap: true,
-    no_intra_emphasis: true,
-    tables: true,
-    fenced_code_blocks: true,
-    autolink: true,
-    disable_indented_code_blocks: true,
-    strikethrough: true,
-    superscript: false
-}
+  no_intra_emphasis:            true,
+  tables:                       true,
+  fenced_code_blocks:           true,
+  autolink:                     true,
+  disable_indented_code_blocks: true,
+  strikethrough:                true,
+  superscript:                  false,
+}.freeze
+
+MARKDOWN_RENDERER_OPTS = {
+  filter_html:     true,
+  escape_html:     true,
+  no_images:       true,
+  no_styles:       true,
+  safe_links_only: true,
+  xhtml:           false,
+  hard_wrap:       true,
+}.freeze
 
 RAW_MARKDOWN_OPTS = {
-  tables: true,
-  fenced_code_blocks: true,
-  autolink: true,
+  tables:                       true,
+  fenced_code_blocks:           true,
+  autolink:                     true,
   disable_indented_code_blocks: true,
-  strikethrough: true,
-  superscript: false
-}
+  strikethrough:                true,
+  superscript:                  false,
+}.freeze
 
 ALLOWED_HOSTS_IN_MARKDOWN = [
-  APP_CONFIG['hostname'],
-  *APP_CONFIG['allowed_hosts_in_markdown']
-]
+  APP_CONFIG["hostname"],
+  *APP_CONFIG["allowed_hosts_in_markdown"]
+].freeze
