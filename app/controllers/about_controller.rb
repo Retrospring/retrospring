0402739e2cf7492ef6d7a2f259d02392bbde71c7
Karina Kwiatek <kjk@kjk.dog>
2023-02-19 18:54:40 +0100

    Fix unclear syntax on about page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -4,10 +4,10 @@ class AboutController < ApplicationController
   def index; end
 
   def about
-    @users = Rails.cache.fetch "about_count_users", expires_in: 1.hour { user_count - current_ban_count }
-    @questions = Rails.cache.fetch "about_count_questions", expires_in: 1.hour { Question.count(:id) }
-    @answers = Rails.cache.fetch "about_count_answers", expires_in: 1.hour { Answer.count(:id) }
-    @comments = Rails.cache.fetch "about_count_comments", expires_in: 1.hour { Comment.count(:id) }
+    @users = Rails.cache.fetch("about_count_users", expires_in: 1.hour) { user_count - current_ban_count }
+    @questions = Rails.cache.fetch("about_count_questions", expires_in: 1.hour) { Question.count(:id) }
+    @answers = Rails.cache.fetch("about_count_answers", expires_in: 1.hour) { Answer.count(:id) }
+    @comments = Rails.cache.fetch("about_count_comments", expires_in: 1.hour) { Comment.count(:id) }
   end
 
   def privacy_policy; end
