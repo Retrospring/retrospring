0451e2fedd40365d354f22f89f783b4ffcad1136
Georg Gadinger <nilsding@nilsding.org>
2023-02-19 21:15:56 +0100

    actions/_share: add telegram

---
+++ spec/views/actions/_share.html.haml_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "actions/_share.html.haml", type: :view do
+  let(:answer) { FactoryBot.create(:answer, user: FactoryBot.create(:user)) }
+
+  subject(:rendered) do
+    render partial: "actions/share", locals: {
+      answer:,
+    }
+  end
+
+  it "has a dropdown item to share to twitter" do
+    expect(rendered).to have_css(%(a.dropdown-item[href^="https://twitter.com/"][target="_blank"]))
+  end
+
+  it "has a dropdown item to share to tumblr" do
+    expect(rendered).to have_css(%(a.dropdown-item[href^="https://www.tumblr.com/"][target="_blank"]))
+  end
+
+  it "has a dropdown item to share to telegram" do
+    expect(rendered).to have_css(%(a.dropdown-item[href^="https://t.me/"][target="_blank"]))
+  end
+
+  it "has a dropdown item to share to anywhere else" do
+    expect(rendered).to have_css(%(a.dropdown-item[name="ab-share"]))
+  end
+end
