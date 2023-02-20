6f56cdc832dac57f06dd29eea88af905cc4fc389
Georg Gadinger <nilsding@nilsding.org>
2023-02-20 21:06:10 +0100

    filter out non-anon questions from blocked/muted users in home timeline

--- app/models/user/relationship/block.rb
+++ app/models/user/relationship/block.rb
@@ -21,17 +21,31 @@ class User
         raise Errors::BlockingSelf if target_user == self
 
         unfollow_and_remove(target_user)
-        create_relationship(active_block_relationships, target_user)
+        create_relationship(active_block_relationships, target_user).tap do
+          expire_blocked_user_ids_cache
+        end
       end
 
       # Unblock an user
       def unblock(target_user)
-        destroy_relationship(active_block_relationships, target_user)
+        destroy_relationship(active_block_relationships, target_user).tap do
+          expire_blocked_user_ids_cache
+        end
       end
 
       # Is <tt>self</tt> blocking <tt>target_user</tt>?
       def blocking?(target_user)
         relationship_active?(blocked_users, target_user)
+      end
+
+      # Expire the blocked user ids cache
+      def expire_blocked_user_ids_cache = Rails.cache.delete(cache_key_blocked_user_ids)
+
+      # Cached ids of the blocked users
+      def blocked_user_ids_cached
+        Rails.cache.fetch(cache_key_blocked_user_ids, expires_in: 1.hour) do
+          blocked_user_ids
+        end
       end
 
       private
@@ -43,6 +57,8 @@ class User
         inboxes.joins(:question).where(questions: { user_id: target_user.id, author_is_anonymous: false }).destroy_all
         ListMember.joins(:list).where(list: { user_id: target_user.id }, user_id: id).destroy_all
       end
+
+      def cache_key_blocked_user_ids = "#{cache_key}/blocked_user_ids"
     end
   end
 end
