2f81d15563c2726ba395e8f807502650d2f5b9d3
Karina Kwiatek <kjk@kjk.dog>
2023-02-21 09:50:28 +0100

    Add tests for `QuestionController`

---
+++ spec/controllers/question_controller_spec.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe QuestionController, type: :controller do
+  describe "#show" do
+    subject { get :show, params: { id: question.id, username: question.user.screen_name } }
+
+    before do
+      stub_const("APP_CONFIG", {
+        "site_name"      => "Specspring",
+        "hostname"       => "test.host",
+        "https"          => false,
+        "items_per_page" => 10,
+      })
+    end
+
+    context "question exists" do
+      let(:question) { FactoryBot.create(:question, user: FactoryBot.create(:user)) }
+
+      context "no answers" do
+        it "renders an empty list" do
+          expect(subject).to have_http_status(:ok)
+          expect(assigns(:question)).to eq(question)
+          expect(assigns(:answers)).to be_empty
+          expect(assigns(:answers_last_id)).to be_nil
+          expect(assigns(:more_data_available)).to eq(false)
+        end
+      end
+
+      context "some answers" do
+        before do
+          num_answers.times do
+            FactoryBot.create(:answer, question:, user: FactoryBot.create(:user))
+          end
+        end
+
+        let(:num_answers) { 10 }
+
+        it "renders a list of questions" do
+          expect(subject).to have_http_status(:ok)
+          expect(assigns(:question)).to eq(question)
+          expect(assigns(:answers).length).to eq(10)
+          expect(assigns(:answers_last_id)).to_not be_nil
+          expect(assigns(:more_data_available)).to eq(false)
+        end
+
+        context "enough answers to paginate" do
+          let(:num_answers) { 11 }
+
+          it "renders a list of questions" do
+            expect(subject).to have_http_status(:ok)
+            expect(assigns(:question)).to eq(question)
+            expect(assigns(:answers).length).to eq(10)
+            expect(assigns(:answers_last_id)).to_not be_nil
+            expect(assigns(:more_data_available)).to eq(true)
+          end
+        end
+      end
+    end
+  end
+end
