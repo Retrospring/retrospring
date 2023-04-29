79e742ce1e2d715ca54fdf1cccb412ff1698419b
Karina Kwiatek <kjk@kjk.dog>
2023-04-29 09:21:48 +0200

    Remove debug

--- app/javascript/retrospring/initializers/hotkey.ts
+++ app/javascript/retrospring/initializers/hotkey.ts
@@ -2,7 +2,6 @@ import { install } from '@github/hotkey'
 
 export default function (): void {
   document.addEventListener('turbo:load', () => {
-    console.debug("Init Hotkey.")
     document.querySelectorAll('[data-hotkey]').forEach(el => install(el as HTMLElement));
   });
 }
