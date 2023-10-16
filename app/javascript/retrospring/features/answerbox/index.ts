3b5cb9d213c97d8023f901acb92df29129c4ed9d
Andreas Nedbal <git@pixelde.su>
2023-10-16 15:19:45 +0200

    Remove answerbox-specific sharing feature

--- app/javascript/retrospring/features/answerbox/index.ts
+++ app/javascript/retrospring/features/answerbox/index.ts
@@ -2,13 +2,11 @@ import registerEvents from 'utilities/registerEvents';
 import registerAnswerboxCommentEvents from './comment';
 import { answerboxDestroyHandler } from './destroy';
 import { answerboxReportHandler } from './report';
-import { shareEventHandler } from './share';
 import { answerboxSmileHandler } from './smile';
 import { answerboxSubscribeHandler } from './subscribe';
 
 export default (): void => {
   registerEvents([
-    { type: 'click', target: '[name=ab-share]', handler: shareEventHandler, global: true },
     { type: 'click', target: '[data-action=ab-submarine]', handler: answerboxSubscribeHandler, global: true },
     { type: 'click', target: '[data-action=ab-report]', handler: answerboxReportHandler, global: true },
     { type: 'click', target: '[data-action=ab-destroy]', handler: answerboxDestroyHandler, global: true },
