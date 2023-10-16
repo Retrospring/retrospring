cbeb613e09ee95bea5c48790f71fb8718d1efe5c
Andreas Nedbal <git@pixelde.su>
2023-10-16 15:20:04 +0200

    Add Stimulus sharing feature to answerboxes

--- app/helpers/social_helper.rb
+++ app/helpers/social_helper.rb
@@ -4,4 +4,13 @@ module SocialHelper
   include SocialHelper::TwitterMethods
   include SocialHelper::TumblrMethods
   include SocialHelper::TelegramMethods
+
+  def answer_share_url(answer)
+    answer_url(
+      id:       answer.id,
+      username: answer.user.screen_name,
+      host:     APP_CONFIG["hostname"],
+      protocol: (APP_CONFIG["https"] ? :https : :http)
+    )
+  end
 end
