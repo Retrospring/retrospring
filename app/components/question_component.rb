07a423bba053037f2e3b14f663b863bfb2e9e380
Andreas Nedbal <git@pixelde.su>
2023-10-21 00:25:08 +0200

    Add `QuestionComponent`

---
+++ app/components/question_component.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class QuestionComponent < ApplicationComponent
+  include ApplicationHelper
+  include BootstrapHelper
+  include UserHelper
+
+  def initialize(question:, context_user: nil, collapse: true, hide_avatar: false, profile_question: false)
+    @question = question
+    @context_user = context_user
+    @collapse = true
+    @hide_avatar = hide_avatar
+    @profile_question = profile_question
+  end
+
+  private
+
+  def author_identifier = @question.author_is_anonymous ? @question.author_identifier : nil
+
+  def follower_question? = !@question.author_is_anonymous && !@question.direct && @question.answer_count > 0
+
+  def hide_avatar? = @hide_avatar || @question.author_is_anonymous
+
+end
