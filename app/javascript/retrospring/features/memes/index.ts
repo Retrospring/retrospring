1e80d756f510366f9572f1e3cce900a2149b379b
Andreas Nedbal <git@pixelde.su>
2023-10-21 00:28:50 +0200

    Adjust class references to question CSS classes

--- app/javascript/retrospring/features/memes/index.ts
+++ app/javascript/retrospring/features/memes/index.ts
@@ -4,7 +4,7 @@ export default (): void => {
   cheet('up up down down left right left right b a', () => {
     document.body.classList.add('fa-spin');
 
-    Array.from(document.querySelectorAll('.answerbox__question-text')).forEach((element: HTMLElement) => {
+    Array.from(document.querySelectorAll('.question__text')).forEach((element: HTMLElement) => {
       element.innerText = ':^)';
     });
   });
