4c393defb7901f581b503336ab08bfb2ca775930
Andreas Nedbal <git@pixelde.su>
2023-10-23 00:45:40 +0200

    Add `ModalController#close` action

---
+++ app/controllers/modal_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class ModalController < ApplicationController
+  include ActionView::Helpers::TagHelper
+  include Turbo::FramesHelper
+
+  skip_before_action :find_active_announcements, :banned?
+
+  def close
+    redirect_to root_path unless turbo_frame_request?
+    render inline: turbo_frame_tag("modal")
+  end
+end
