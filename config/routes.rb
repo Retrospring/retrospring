bae36a5aabf80d1dd29d43386f5f2bd540da438d
Andreas Nedbal <git@pixelde.su>
2023-10-23 00:45:00 +0200

    Add `show_reactions` action and move markup into it

--- config/routes.rb
+++ config/routes.rb
@@ -156,6 +156,7 @@ Rails.application.routes.draw do
   get "/@:username/a/:id/comments", to: "comment#index", as: :comments
   get "/@:username/a/:id/reactions", to: "reaction#index", as: :reactions
   get "/@:username/q/:id", to: "question#show", as: :question
+  get "/@:username/c/:id/reactions", to: "comment#show_reactions", as: :comment_reactions
   get "/@:username/followers", to: "user#followers", as: :show_user_followers
   get "/@:username/followings", to: "user#followings", as: :show_user_followings
   get "/@:username/friends", to: redirect("/@%{username}/followings")
