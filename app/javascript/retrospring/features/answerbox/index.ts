42b5b6ccc2e796a4a9bfd236fd3e184b3bb29130
Andreas Nedbal <git@pixelde.su>
2023-10-25 02:52:19 +0200

    Remove TypeScript subscription functionality

--- app/javascript/retrospring/features/answerbox/index.ts
+++ app/javascript/retrospring/features/answerbox/index.ts
@@ -3,11 +3,9 @@ import registerAnswerboxCommentEvents from './comment';
 import { answerboxDestroyHandler } from './destroy';
 import { answerboxReportHandler } from './report';
 import { answerboxSmileHandler } from './smile';
-import { answerboxSubscribeHandler } from './subscribe';
 
 export default (): void => {
   registerEvents([
-    { type: 'click', target: '[data-action=ab-submarine]', handler: answerboxSubscribeHandler, global: true },
     { type: 'click', target: '[data-action=ab-report]', handler: answerboxReportHandler, global: true },
     { type: 'click', target: '[data-action=ab-destroy]', handler: answerboxDestroyHandler, global: true },
     { type: 'click', target: '[name=ab-smile]', handler: answerboxSmileHandler, global: true }
