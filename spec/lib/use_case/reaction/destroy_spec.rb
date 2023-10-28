6e2310eaef340e921ffd40d94803e404733480d4
Andreas Nedbal <git@pixelde.su>
2023-10-28 17:16:17 +0200

    Add tests for reaction use cases

---
+++ spec/lib/use_case/reaction/destroy_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe UseCase::Reaction::Destroy do
+  shared_examples_for "valid target type" do
+    before do
+      user.smile target
+    end
+
+    it "destroys a reaction" do
+      expect { subject }.to change { target.smile_count }.by(-1)
+    end
+  end
+
+  subject { UseCase::Reaction::Destroy.call(source_user: user, target:) }
+
+  let(:user) { FactoryBot.create(:user) }
+
+  context "target is an Answer" do
+    let(:target) { FactoryBot.create(:answer, user:) }
+
+    include_examples "valid target type"
+  end
+
+  context "target is a Comment" do
+    let(:target) { FactoryBot.create(:comment, user:, answer: FactoryBot.create(:answer, user:)) }
+
+    include_examples "valid target type"
+  end
+end
