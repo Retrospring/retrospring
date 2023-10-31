e3187638016f720007d2b78b3f7b8a493b8b406a
Andreas Nedbal <git@pixelde.su>
2023-10-31 23:33:30 +0100

    Remove legacy smile TypeScript functionality

--- app/javascript/retrospring/features/answerbox/index.ts
+++ app/javascript/retrospring/features/answerbox/index.ts
@@ -2,13 +2,11 @@ import registerEvents from 'utilities/registerEvents';
 import registerAnswerboxCommentEvents from './comment';
 import { answerboxDestroyHandler } from './destroy';
 import { answerboxReportHandler } from './report';
-import { answerboxSmileHandler } from './smile';
 
 export default (): void => {
   registerEvents([
     { type: 'click', target: '[data-action=ab-report]', handler: answerboxReportHandler, global: true },
     { type: 'click', target: '[data-action=ab-destroy]', handler: answerboxDestroyHandler, global: true },
-    { type: 'click', target: '[name=ab-smile]', handler: answerboxSmileHandler, global: true }
   ]);
 
   registerAnswerboxCommentEvents();
