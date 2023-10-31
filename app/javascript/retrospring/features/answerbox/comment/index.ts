21cda65ecbb73a83d56ce675deb4987f6eb2cda5
Andreas Nedbal <git@pixelde.su>
2023-10-31 23:33:30 +0100

    Remove legacy smile TypeScript functionality

--- app/javascript/retrospring/features/answerbox/comment/index.ts
+++ app/javascript/retrospring/features/answerbox/comment/index.ts
@@ -2,7 +2,6 @@ import registerEvents from "retrospring/utilities/registerEvents";
 import { commentDestroyHandler } from "./destroy";
 import { commentComposeEnd, commentComposeStart, commentCreateClickHandler, commentCreateKeyboardHandler } from "./new";
 import { commentReportHandler } from "./report";
-import { commentSmileHandler } from "./smile";
 import { commentToggleHandler } from "./toggle";
 import { commentHotkeyHandler } from "retrospring/features/answerbox/comment/hotkey";
 
@@ -10,7 +9,6 @@ export default (): void => {
   registerEvents([
     { type: 'click', target: '[name=ab-comments]', handler: commentToggleHandler, global: true },
     { type: 'click', target: '[name=ab-open-and-comment]', handler: commentHotkeyHandler, global: true },
-    { type: 'click', target: '[name=ab-smile-comment]', handler: commentSmileHandler, global: true },
     { type: 'click', target: '[data-action=ab-comment-report]', handler: commentReportHandler, global: true },
     { type: 'click', target: '[data-action=ab-comment-destroy]', handler: commentDestroyHandler, global: true },
     { type: 'compositionstart', target: '[name=ab-comment-new]', handler: commentComposeStart, global: true },
