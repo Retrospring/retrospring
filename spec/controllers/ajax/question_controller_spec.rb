2327b2ce521b54f99fae553cecb4d89d05ac3a0c
Karina Kwiatek <kjk@kjk.dog>
2023-11-01 22:40:41 +0100

    Update tests for single-recipient QuestionWorker behaviour

--- spec/controllers/ajax/question_controller_spec.rb
+++ spec/controllers/ajax/question_controller_spec.rb
@@ -48,11 +48,13 @@ describe Ajax::QuestionController, :ajax_controller, type: :controller do
       include_examples "returns the expected response"
     end
 
-    shared_examples "enqueues a QuestionWorker job" do |_expected_rcpt|
+    shared_examples "enqueues QuestionWorker jobs" do
       it "enqueues a QuestionWorker job" do
-        allow(QuestionWorker).to receive(:perform_async)
+        allow(QuestionWorker).to receive(:perform_bulk)
         subject
-        expect(QuestionWorker).to have_received(:perform_async).with(user.id, Question.last.id)
+        question_id = Question.last.id
+        bulk_args = followers.map { |f| [f.id, question_id] }
+        expect(QuestionWorker).to have_received(:perform_bulk).with(bulk_args)
       end
 
       include_examples "returns the expected response"
@@ -61,8 +63,10 @@ describe Ajax::QuestionController, :ajax_controller, type: :controller do
     shared_examples "does not enqueue a QuestionWorker job" do
       it "does not enqueue a QuestionWorker job" do
         allow(QuestionWorker).to receive(:perform_async)
+        allow(QuestionWorker).to receive(:perform_bulk)
         subject
         expect(QuestionWorker).not_to have_received(:perform_async)
+        expect(QuestionWorker).not_to have_received(:perform_bulk)
       end
 
       include_examples "returns the expected response"
@@ -194,13 +198,18 @@ describe Ajax::QuestionController, :ajax_controller, type: :controller do
 
       context "when rcpt is followers" do
         let(:rcpt) { "followers" }
+        let(:followers) { FactoryBot.create_list(:user, 3) }
+
+        before do
+          followers.each { |follower| follower.follow(user) }
+        end
 
         context "when anonymousQuestion is true" do
           let(:anonymous_question) { "true" }
           let(:expected_question_anonymous) { false }
 
           include_examples "creates the question", false
-          include_examples "enqueues a QuestionWorker job", "followers"
+          include_examples "enqueues QuestionWorker jobs"
         end
 
         context "when anonymousQuestion is false" do
@@ -208,7 +217,7 @@ describe Ajax::QuestionController, :ajax_controller, type: :controller do
           let(:expected_question_anonymous) { false }
 
           include_examples "creates the question", false
-          include_examples "enqueues a QuestionWorker job", "followers"
+          include_examples "enqueues QuestionWorker jobs"
         end
       end
 
