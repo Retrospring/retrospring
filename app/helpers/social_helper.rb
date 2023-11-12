dee298fe3327b470feefb42df2146c5c74fc8d6b
Ahmed Ejaz <chahmedejaz9050@gmail.com>
2023-11-12 16:06:43 +0500

    1448, remove share#copyToClipboard changes

--- app/helpers/social_helper.rb
+++ app/helpers/social_helper.rb
@@ -13,9 +13,4 @@ module SocialHelper
       protocol: (APP_CONFIG["https"] ? :https : :http),
     )
   end
-
-  def answer_copy_content(answer)
-    # copy content template: "question - answer [link]"
-    "#{answer.question.content} - #{answer.content} [#{answer_share_url(answer)}]"
-  end
 end
