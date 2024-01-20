6912cf72ece849db8c21197886cffc2e3858ebe6
Andreas Nedbal <git@pixelde.su>
2024-01-20 14:49:25 +0100

    Add spec for inbox cleanup scheduler job

---
+++ spec/workers/scheduler/inbox_cleanup_scheduler_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Scheduler::InboxCleanupScheduler do
+  let(:user) { FactoryBot.create(:user) }
+  let(:inbox) { FactoryBot.create(:inbox, user:) }
+
+  describe "#perform" do
+    before do
+      inbox.question_id = nil
+      inbox.save(validate: false)
+    end
+
+    subject { described_class.new.perform }
+
+    it "should delete orphaned inbox entries" do
+      expect { subject }
+        .to(
+          change { Inbox.where(question_id: nil).count }
+            .from(1)
+            .to(0),
+        )
+    end
+  end
+end
