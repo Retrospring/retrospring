a9e4265a1a344e60686b13ebf2d8b45be3674ca3
Andreas Nedbal <git@pixelde.su>
2024-01-20 14:48:57 +0100

    Add scheduled job to clean up orphaned inbox entries

---
+++ app/workers/scheduler/inbox_cleanup_scheduler.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Scheduler::InboxCleanupScheduler
+  include Sidekiq::Worker
+
+  sidekiq_options retry: false
+
+  def perform
+    orphaned_entries = Inbox.where(question_id: nil).includes(:user)
+    orphaned_entries.each do |inbox|
+      logger.info "Deleting orphaned inbox entry #{inbox.id} from user #{inbox.user.id}"
+      inbox.destroy
+    end
+  end
+end
