175a044fbf594f1abb93f6e57b37498fe8e6724f
Andreas Nedbal <git@pixelde.su>
2024-03-01 22:09:37 +0100

    Add `last_reports_visit` field to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_27_112216) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_01_203930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -369,6 +369,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_27_112216) do
     t.string "sharing_custom_url"
     t.datetime "notifications_updated_at", precision: nil
     t.datetime "inbox_updated_at", precision: nil
+    t.datetime "last_reports_visit"
     t.index "lower((screen_name)::text)", name: "index_users_on_LOWER_screen_name", unique: true
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
