8e8a9e878e4d847231aae5edc636eeb248a94653
Andreas Nedbal <git@pixelde.su>
2024-03-01 20:54:46 +0100

    Add specs for `SocialHelper::BlueskyMethods`

---
+++ spec/helpers/social_helper/bluesky_methods_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe SocialHelper::BlueskyMethods, type: :helper do
+  let(:user) { FactoryBot.create(:user) }
+  let(:question_content) { "q" * 255 }
+  let(:answer_content) { "a" * 255 }
+  let(:answer) do
+    FactoryBot.create(:answer, user:,
+                               content:          answer_content,
+                               question_content:,)
+  end
+
+  before do
+    stub_const("APP_CONFIG", {
+                 "hostname"       => "example.com",
+                 "https"          => true,
+                 "items_per_page" => 5,
+               },)
+  end
+
+  describe "#bluesky_share_url" do
+    subject { bluesky_share_url(answer) }
+
+    it "should return a proper share link" do
+      expect(subject).to eq("https://bsky.app/intent/compose?text=#{CGI.escape(prepare_tweet(answer))}")
+    end
+  end
+end
