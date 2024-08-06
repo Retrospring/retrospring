0e9878a21d4ec952799e3753a0759569d15aeb0b
Andreas Nedbal <andy@pixelde.su>
2024-08-06 22:44:07 +0200

    Represent theme colors as hexadecimal in export

--- lib/use_case/data_export/theme.rb
+++ lib/use_case/data_export/theme.rb
@@ -3,20 +3,24 @@
 module UseCase
   module DataExport
     class Theme < UseCase::DataExport::Base
+      include ThemeHelper
+
+      MODEL_FIELDS = %i[id user_id created_at updated_at].freeze
+
       def files
         return {} unless user.theme
 
         {
           "theme.json" => json_file!(
-            theme: theme_data
-          )
+            theme: theme_data,
+          ),
         }
       end
 
       def theme_data
         {}.tap do |obj|
-          column_names(::Theme).each do |field|
-            obj[field] = user.theme[field]
+          (column_names(::Theme) - MODEL_FIELDS).each do |field|
+            obj[field] = "##{get_hex_color_from_theme_value(user.theme[field])}"
           end
         end
       end
