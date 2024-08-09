268df27358815f17f6d1d29452170f9ca675665a
Andreas Nedbal <andy@pixelde.su>
2024-08-09 05:54:04 +0200

    Add specs to check for dis/enabled registrations

---
+++ spec/views/about/index.html.haml_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "about/index.html.haml", type: :view do
+  before do
+    stub_const("APP_CONFIG", {
+                 "hostname" => "example.com",
+                 "https"    => true,
+                 "sitename" => "yastask",
+               },)
+  end
+
+  subject(:rendered) { render }
+
+  context "registrations are enabled" do
+    before do
+      allow(APP_CONFIG).to receive(:dig).with(:features, :registration, :enabled).and_return(true)
+    end
+
+    it "has references to registering now" do
+      expect(rendered).to match(/Register now/)
+    end
+  end
+
+  context "registrations are disabled" do
+    before do
+      allow(APP_CONFIG).to receive(:dig).with(:features, :registration, :enabled).and_return(false)
+    end
+
+    it "has no references to registering now" do
+      expect(rendered).to_not match(/Register now/)
+    end
+  end
+end
---
+++ spec/views/navigation/_guest.html.haml_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "navigation/_guest.html.haml", type: :view do
+  subject(:rendered) do
+    render partial: "navigation/guest"
+  end
+
+  context "registrations are enabled" do
+    before do
+      allow(APP_CONFIG).to receive(:dig).with(:features, :registration, :enabled).and_return(true)
+    end
+
+    it "has a sign up link" do
+      expect(rendered).to match(/Sign up/)
+    end
+  end
+
+  context "registrations are disabled" do
+    before do
+      allow(APP_CONFIG).to receive(:dig).with(:features, :registration, :enabled).and_return(false)
+    end
+
+    it "has no sign up link" do
+      expect(rendered).to_not match(/Sign up/)
+    end
+  end
+end
