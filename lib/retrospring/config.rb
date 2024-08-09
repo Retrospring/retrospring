ca98b9dd7f96690b99914496259961808f7643d7
Jyrki Gadinger <nilsding@nilsding.org>
2024-08-09 11:12:38 +0200

    move site config from initialiser to own module for potential improvements in how to access common configs when needed ...  way better than using `.dig` by hand everywhere i'd say

---
+++ lib/retrospring/config.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Retrospring
+  module Config
+    module_function
+
+    def config_hash = {}.with_indifferent_access.tap do |hash|
+      # load yml config if it's present
+      justask_yml_path = Rails.root.join("config/justask.yml")
+      hash.merge!(YAML.load_file(justask_yml_path)) if File.exist?(justask_yml_path)
+
+      # load config from ENV where possible
+      env_config = {
+        # The site name, shown everywhere
+        site_name: ENV.fetch("SITE_NAME", nil),
+
+        hostname:  ENV.fetch("HOSTNAME", nil),
+      }.compact
+      hash.merge!(env_config)
+
+      # Update rails config for mail
+      Rails.application.config.action_mailer.default_url_options = {
+        host: hash["hostname"],
+      }
+    end
+
+    def registrations_enabled? = APP_CONFIG.dig(:features, :registration, :enabled)
+  end
+end
