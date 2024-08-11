784e9e0a2929863a62fec3e216d6db554af5b7c1
Andreas Nedbal <andy@pixelde.su>
2024-08-11 05:25:07 +0200

    Add specs for correct template in about controller

--- spec/controllers/about_controller_spec.rb
+++ spec/controllers/about_controller_spec.rb
@@ -3,6 +3,32 @@
 require "rails_helper"
 
 describe AboutController, type: :controller do
+  describe "#index" do
+    subject { get :index }
+
+    context "advanced layout is enabled" do
+      before do
+        allow(APP_CONFIG).to receive(:dig).with(:features, :advanced_frontpage, :enabled).and_return(true)
+      end
+
+      it "renders the correct template" do
+        subject
+        expect(response).to render_template("about/index_advanced")
+      end
+    end
+
+    context "advanced layout is disabled" do
+      before do
+        allow(APP_CONFIG).to receive(:dig).with(:features, :advanced_frontpage, :enabled).and_return(false)
+      end
+
+      it "renders the correct template" do
+        subject
+        expect(response).to render_template("about/index")
+      end
+    end
+  end
+
   describe "#about" do
     subject { get :about }
 
