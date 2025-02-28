d2568d9065f1e5e6ada0527ab7745177a4412598
Andreas Nedbal <andy@pixelde.su>
2025-02-28 20:53:12 +0100

    Add customizable source URL

--- lib/retrospring/version.rb
+++ lib/retrospring/version.rb
@@ -25,6 +25,8 @@ module Retrospring
 
     def minor = [month.to_s.rjust(2, "0"), day.to_s.rjust(2, "0")].join
 
+    def source_url = APP_CONFIG.dig(:source_url) || "https://github.com/retrospring/retrospring"
+
     def to_a = [year.to_s, minor, patch.to_s]
 
     def to_s = [to_a.join("."), suffix].join
