68af94f49a36719503d32040c50d2375fab68544
Andreas Nedbal <andy@pixelde.su>
2025-02-28 21:34:44 +0100

    Remove now unused IO markdown methods and specs

--- spec/helpers/markdown_helper_spec.rb
+++ spec/helpers/markdown_helper_spec.rb
@@ -85,40 +85,4 @@ describe MarkdownHelper, type: :helper do
       expect(raw_markdown("# Heading")).to eq("<h1>Heading</h1>\n")
     end
   end
-
-  describe "#get_markdown" do
-    it "should return the contents of the specified file" do
-      expect(get_markdown("spec/fixtures/markdown/test.md")).to eq("# Heading")
-    end
-
-    it "should return an error message on error" do
-      expect(get_markdown("non/existant/path.md")).to eq("# Error reading #{Rails.root.join('non/existant/path.md')}")
-    end
-  end
-
-  describe "#markdown_io" do
-    it "should return the expected text" do
-      expect(markdown_io("spec/fixtures/markdown/io.md")).to eq("<p><strong>Strong text</strong></p>")
-    end
-  end
-
-  describe "#strip_markdown_io" do
-    it "should return the expected text" do
-      expect(strip_markdown_io("spec/fixtures/markdown/io.md")).to eq("Strong text")
-    end
-  end
-
-  describe "#twitter_markdown_io" do
-    let(:user) { FactoryBot.create(:user) }
-
-    it "should return the expected text" do
-      expect(twitter_markdown_io("spec/fixtures/markdown/twitter.md")).to eq("test")
-    end
-  end
-
-  describe "#raw_markdown_io" do
-    it "should return the expected text" do
-      expect(raw_markdown_io("spec/fixtures/markdown/test.md")).to eq("<h1>Heading</h1>\n")
-    end
-  end
 end
