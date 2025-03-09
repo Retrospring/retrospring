c3d7f5dcd0cda6ea6153d04d25c8210b831d8a48
Andreas Nedbal <andy@pixelde.su>
2025-03-09 23:22:43 +0100

    Okay, SanitizeHelper maybe?

--- app/helpers/application_helper/title_methods.rb
+++ app/helpers/application_helper/title_methods.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper::TitleMethods
-  include ActionView::Helpers::TextHelper
+  include ActionView::Helpers::SanitizeHelper 
   include MarkdownHelper
   include UserHelper
 
