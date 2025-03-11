f24e12474d59334c39da3d4ea6b98c601de6841d
Andreas Nedbal <andy@pixelde.su>
2025-03-12 00:31:07 +0100

    Move SanitizeHelper include into ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include ActionView::Helpers::SanitizeHelper
   include Pundit::Authorization
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
